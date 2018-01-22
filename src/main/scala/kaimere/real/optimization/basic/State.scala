54523fe8cf11e131966e0b0f04c16e94d32b19b6
wol4aravio <wol4aravio@yandex.ru>
2018-01-22 07:26:41 +0300

    State

---
+++ src/main/scala/kaimere/real/optimization/basic/State.scala
@@ -0,0 +1,13 @@
+package kaimere.real.optimization.basic
+
+import kaimere.real.objects.{RealVector, Function}
+
+case class State(components: Vector[RealVector]) extends Traversable[RealVector] {
+
+  final override def foreach[U](f: RealVector => U): Unit = components.foreach(f)
+
+  final def apply(id: Int): RealVector = components(id)
+
+  final def getBestBy(f: Function): RealVector = this.minBy(x => f(x))
+
+}
