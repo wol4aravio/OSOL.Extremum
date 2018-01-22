86651ae2cef5824a1ce3ee2c0ffbdd4c2ae01723
wol4aravio <wol4aravio@yandex.ru>
2018-01-22 22:54:27 +0300

    State

--- src/main/scala/kaimere/real/optimization/general/State.scala
+++ src/main/scala/kaimere/real/optimization/general/State.scala
@@ -1,6 +1,9 @@
 package kaimere.real.optimization.general
 
 import kaimere.real.objects.{RealVector, Function}
+import kaimere.real.objects.RealVector._
+
+import spray.json._
 
 case class State(components: Vector[RealVector]) extends Traversable[RealVector] {
 
@@ -9,5 +12,26 @@ case class State(components: Vector[RealVector]) extends Traversable[RealVector]
   final def apply(id: Int): RealVector = components(id)
 
   final def getBestBy(f: Function): RealVector = this.minBy(x => f(x))
+
+}
+
+object State {
+
+  implicit object StateJsonFormat extends RootJsonFormat[State] {
+    def write(s: State) =
+      JsObject("components" -> JsArray(s.components.map(v => v.toJson)))
+
+    def read(json: JsValue): State =
+      json.asJsObject.getFields("components") match {
+        case Seq(JsArray(jsonComponents)) =>
+          val components = jsonComponents
+            .map {
+              case v => v.convertTo[RealVector]
+              case _ => throw DeserializationException("RealVector expected")
+            }
+          State(components)
+        case _ => throw DeserializationException("State expected")
+      }
+  }
 
 }
