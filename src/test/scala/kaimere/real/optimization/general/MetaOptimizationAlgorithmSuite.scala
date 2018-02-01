94df79c713ff2d74e0467db9acf2f6af774cdee6
wol4aravio <wol4aravio@yandex.ru>
2018-02-01 17:02:26 +0300

    MOA

--- src/test/scala/kaimere/real/optimization/general/MetaOptimizationAlgorithmSuite.scala
+++ src/test/scala/kaimere/real/optimization/general/MetaOptimizationAlgorithmSuite.scala
@@ -4,6 +4,7 @@ import kaimere.real.optimization._
 import kaimere.real.optimization.general.instructions._
 import kaimere.real.optimization.classic.zero_order.RandomSearch
 import org.scalatest.FunSuite
+import spray.json._
 
 class MetaOptimizationAlgorithmSuite extends FunSuite {
 
@@ -12,15 +13,20 @@ class MetaOptimizationAlgorithmSuite extends FunSuite {
   private val maxTime = 0.5
 
   private val rs: OptimizationAlgorithm = RandomSearch(10, 0.01)
-  private val SA: OptimizationAlgorithm = MetaOptimizationAlgorithm(
+  private val MOA: OptimizationAlgorithm = MetaOptimizationAlgorithm(
     algorithms = Seq(rs, rs, rs),
     targetVars = Seq(Some(Set("x", "a")), Some(Set("y", "b")), Some(Set("z", "c"))),
     instructions = Seq(MaxTime(maxTime), MaxTime(maxTime), MaxTime(maxTime)))
 
+  test("Serialization") {
+    assert(MOA.asInstanceOf[MetaOptimizationAlgorithm].toJson.convertTo[MetaOptimizationAlgorithm] == MOA.asInstanceOf[MetaOptimizationAlgorithm])
+  }
+
+
   test("Dummy #4 (by max time)") {
 
     val passed = Tester(
-      tool = SA,
+      tool = MOA,
       f = DummyFunctions.func_4,
       area = DummyFunctions.area_4,
       state = None,
