88682b438716120698c2e78320bf35462a87117d
wol4aravio <wol4aravio@yandex.ru>
2018-02-02 01:57:49 +0300

    Fixes

--- src/test/scala/kaimere/real/optimization/metaheuristic/CatSwarmOptimizationSuite.scala
+++ src/test/scala/kaimere/real/optimization/metaheuristic/CatSwarmOptimizationSuite.scala
@@ -36,14 +36,14 @@ class CatSwarmOptimizationSuite extends FunSuite {
 
   }
 
-  test("Dummy #1 (by target value)") {
+  test("Dummy #1 (by max iterations)") {
 
     val passed = Tester(
       tool = CSO,
       f = DummyFunctions.func_1,
       area = DummyFunctions.area_1,
       state = Some(Vector(Map("x" -> 10.0))),
-      instruction = TargetValue(targetValue = 0.0),
+      instruction = MaxIterations(1 * maxIterations),
       epsNorm = epsNorm,
       maxTries = maxTries)
 
