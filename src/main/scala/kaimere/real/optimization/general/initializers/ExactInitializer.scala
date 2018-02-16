88049fbf4790cce9ac9d0f2433a99e3763484ef9
wol4aravio <wol4aravio@yandex.ru>
2018-02-17 01:41:00 +0300

    Initializer mod

--- src/main/scala/kaimere/real/optimization/general/initializers/ExactInitializer.scala
+++ src/main/scala/kaimere/real/optimization/general/initializers/ExactInitializer.scala
@@ -2,9 +2,9 @@ package kaimere.real.optimization.general.initializers
 import kaimere.real.optimization.general.OptimizationAlgorithm
 import kaimere.tools.random.GoRN
 
-case class ExactInitializer(target: Double) extends Initializer {
+case class ExactInitializer(defaultValue: Double, keyValuePair: (String, Double)*) extends Initializer {
 
   override def generateState(algorithm: OptimizationAlgorithm): Vector[Map[String, Double]] =
-    Vector(algorithm.area.map { case (key, _) => (key, target) })
+    Vector(algorithm.area.map { case (key, _) => (key, keyValuePair.toMap.getOrElse(key, defaultValue)) })
 
 }
