33e51059cf9aafe978d4e05cf971efeaf014090d
wol4aravio <wol4aravio@yandex.ru>
2018-03-29 09:54:47 +0300

    Purge

--- src/main/scala/kaimere/real/optimization/general/initializers/Initializer.scala
+++
@@ -1,9 +0,0 @@
-package kaimere.real.optimization.general.initializers
-
-import kaimere.real.optimization.general.{OptimizationAlgorithm, State}
-
-trait Initializer {
-
-  def generateState(algorithm: OptimizationAlgorithm): State
-
-}
