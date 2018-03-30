1662291f51537fb01a832e38cbabc9b3529cb12b
wol4aravio <wol4aravio@yandex.ru>
2018-03-30 11:39:27 +0300

    RealVector

---
+++ Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/VectorObjects/RealVector.scala
@@ -0,0 +1,78 @@
+package OSOL.Extremum.Core.Scala.VectorObjects
+
+import RealVector.Converters._
+import OSOL.Extremum.Core.Scala.CodeFeatures.Pipe
+
+/** Ordinary numerical vector
+  *
+  * @param vals values which form VectorObject (key-value pairs)
+  */
+class RealVector private (override val vals: Map[String, Double]) extends VectorObject[Double](vals) {
+
+  final override def add(that: VectorObject[Double]): RealVector =
+    this.elementWiseOp(that, (a, b) => a + b)
+
+  final override def addImputeMissingKeys(that: VectorObject[Double]): RealVector =
+    this.elementWiseOpImputeMissingKeys(that, (a, b) => a + b, 0.0)
+
+  final override def multiply(that: VectorObject[Double]): RealVector =
+    this.elementWiseOp(that, (a, b) => a * b)
+
+  final override def multiplyImputeMissingKeys(that: VectorObject[Double]): RealVector =
+    this.elementWiseOpImputeMissingKeys(that, (a, b) => a * b, 1.0)
+
+  final override def multiply(coefficient: Double): RealVector =
+    this.keys.map(k => (k, coefficient * this (k))) |> RealVector.apply
+
+  final override def moveBy(delta: Map[String, Double]): RealVector = {
+    val deltaWithImputedValues = delta ++ this.keys.diff(delta.keySet).map(k => (k, 0.0))
+    this.add(deltaWithImputedValues |> RealVector.apply)
+  }
+
+  final override def constrain(area: Map[String, (Double, Double)]): RealVector = {
+    val constrainedVector = this.vals.map { case (k, value) => (k,
+      k match {
+        case _ if area.isDefinedAt(k) =>
+          val (min, max) = area(k)
+          if (value > max) max
+          else {
+            if (value < min) min
+            else value
+          }
+        case _ => value
+      })
+    }
+    constrainedVector
+  }
+}
+
+/** Companion object for [[OSOL.Extremum.Core.Scala.VectorObjects.RealVector RealVector]] class */
+object RealVector {
+
+  /** Implicit converters for [[OSOL.Extremum.Core.Scala.VectorObjects.RealVector RealVector]] */
+  object Converters {
+
+    /** Converts `Iterable[(String, Double)]` to [[OSOL.Extremum.Core.Scala.VectorObjects.RealVector RealVector]]
+      *
+      * @param v `(key, value)` pairs
+      * @return [[OSOL.Extremum.Core.Scala.VectorObjects.RealVector RealVector]]
+      */
+    implicit def Iterable_to_RealVector(v: Iterable[(String, Double)]): RealVector = RealVector(v)
+
+  }
+
+  /** Create object from `(key, value)` pairs
+    *
+    * @param keyValuePairs target `(key, value)` pairs
+    * @return vector composed of `keyValuePairs`
+    */
+  def apply(keyValuePairs: (String, Double)*): RealVector = new RealVector(keyValuePairs.toMap)
+  /** Create object from `(key, value)` pairs
+    *
+    * @param keyValuePairs target `(key, value)` pairs
+    * @return vector composed of `keyValuePairs`
+    */
+  def apply(keyValuePairs: Iterable[(String, Double)]): RealVector = RealVector(keyValuePairs.toSeq:_*)
+
+
+}
