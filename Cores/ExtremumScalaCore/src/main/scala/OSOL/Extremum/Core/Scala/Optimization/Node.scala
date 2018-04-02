285f2d88833cd103df424230446d6e50c1f21c15
wol4aravio <wol4aravio@yandex.ru>
2018-04-02 07:51:28 +0300

    Node to GeneralNode

--- Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/Node.scala
+++
@@ -1,9 +0,0 @@
-package OSOL.Extremum.Core.Scala.Optimization
-
-trait Node[Base, FuncType, V <: Optimizable[Base, FuncType]] {
-
-  def process(s: State[Base, FuncType, V]): Unit
-
-  def getNextNode(s: State[Base, FuncType, V]): Option[Int] = None
-
-}
