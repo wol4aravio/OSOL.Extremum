54bedfe0adb270fb17922cd9de48960e31d529a9
wol4aravio <wol4aravio@yandex.ru>
2018-04-02 08:46:43 +0300

    Exceptions

--- Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/package.scala
+++ Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/package.scala
@@ -4,4 +4,11 @@ package object Optimization {
 
   type Area = Map[String, (Double, Double)]
 
+  object Exceptions {
+
+    class ParameterAlreadyExistsException(parameterName: String) extends Exception
+    class NoSuchParameterException(parameterName: String) extends Exception
+
+  }
+
 }
