4b472a3276141c626820653aa73d926d82e25e62
wol4aravio <wol4aravio@yandex.ru>
2018-04-02 07:51:45 +0300

    GeneralNode

---
+++ Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/Nodes/GeneralNode.scala
@@ -0,0 +1,13 @@
+package OSOL.Extremum.Core.Scala.Optimization.Nodes
+
+import OSOL.Extremum.Core.Scala.Optimization._
+
+abstract class GeneralNode[Base, FuncType, V <: Optimizable[Base, FuncType]](val nodeId: Int) {
+
+  def initialize(f: Map[String, FuncType] => FuncType, area: Area, s: State[Base, FuncType, V]): Unit
+
+  def process(f: Map[String, FuncType] => FuncType, area: Area, s: State[Base, FuncType, V]): Unit
+
+  def getNextNode(f: Map[String, FuncType] => FuncType, area: Area, s: State[Base, FuncType, V]): Option[Int] = None
+
+}
