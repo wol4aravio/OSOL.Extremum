03b82a0a69265feba61a8a3ca7af06b1df788538
wol4aravio <wol4aravio@yandex.ru>
2018-04-02 07:51:53 +0300

    Ready for Test

--- Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/Algorithm.scala
+++ Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/Algorithm.scala
@@ -1,5 +1,28 @@
 package OSOL.Extremum.Core.Scala.Optimization
 
-class Algorithm[Base, FuncType, V <: Optimizable[Base, FuncType]] {
+import scala.util.control.Breaks._
+import OSOL.Extremum.Core.Scala.Optimization.Nodes.GeneralNode
+
+final class Algorithm[Base, FuncType, V <: Optimizable[Base, FuncType]] private
+(nodes: Seq[GeneralNode[Base, FuncType, V]], transitionMatrix: Seq[(Int, Option[Int], Int)]) {
+
+  private var state: State[Base, FuncType, V] = null
+
+  private var currentNode: GeneralNode[Base, FuncType, V] = null
+
+  def initialize(f: Map[String, FuncType] => FuncType, area: Area): Unit = nodes.foreach(_.initialize(f, area, state))
+
+  def work(f: Map[String, FuncType] => FuncType, area: Area): V = {
+    initialize(f, area)
+    while (true) {
+      currentNode.process(f, area, state)
+      val currentConditionValue = currentNode.getNextNode(f, area, state)
+      val nextNode = transitionMatrix.find { case (currentId, condition, _) =>
+        currentId == currentNode.nodeId && condition == currentConditionValue }
+      if (nextNode.isDefined) { currentNode = nodes.find(_.nodeId == nextNode).get }
+      else break
+    }
+    state.elements.minBy(_.getPerformance(f))
+  }
 
 }
--- Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/State.scala
+++ Cores/ExtremumScalaCore/src/main/scala/OSOL/Extremum/Core/Scala/Optimization/State.scala
@@ -4,6 +4,8 @@ import scala.reflect.ClassTag
 
 class State[Base, FuncType, V <: Optimizable[Base, FuncType]](var elements: Array[V]) {
 
+  final lazy val capacity = elements.length
+
   final def apply(id: Int): V = elements(id)
 
   final def changeElement(id: Int, vNew: V): Unit = {
@@ -15,5 +17,7 @@ class State[Base, FuncType, V <: Optimizable[Base, FuncType]](var elements: Arra
   final def changeElements(elementsNew: Iterable[V])(implicit c: ClassTag[V]): Unit = {
     this.elements = elementsNew.toArray
   }
+
+  final val parameters: scala.collection.mutable.Map[String, Any] = scala.collection.mutable.Map.empty[String, Any]
 
 }
