b86a9c43ccf8d38deba772b417cb529a406f5988
wol4aravio <wol4aravio@yandex.ru>
2018-04-05 15:47:55 +0300

    Begining

---
+++ Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/VectorExceptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OSOL.Extremum.Core.DotNet.Vectors
+{
+    public static class VectorExceptions
+    {
+        public class MissingKeyException : Exception
+        {
+            private readonly string _missingKey;
+
+            public MissingKeyException(string missingKey)
+            {
+                this._missingKey = string.Copy(missingKey);
+            }
+
+            public override string Message => $"{_missingKey} is not present in current VectorObject";
+        }
+
+        public class DifferentKeysException : Exception
+        {
+            private readonly string[] _keys_1, _keys_2;
+
+            public DifferentKeysException(string[] keys_1, string[] keys_2)
+            {
+                this._keys_1 = new string[keys_1.Length];
+                this._keys_2 = new string[keys_2.Length];
+                
+                Array.Copy(keys_1, _keys_1, keys_1.Length);
+                Array.Copy(keys_2, _keys_2, keys_2.Length);
+            }
+
+            public override string Message => $"VectorObjects have different set of keys: {_keys_1} and {_keys_2}";
+        }
+    }
+}
---
+++ Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/VectorObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace OSOL.Extremum.Core.DotNet.Vectors
+{
+    public abstract class VectorObject<TBase>
+    {
+        public Dictionary<string, TBase> Elements;
+
+        public IEnumerable<string> Keys => this.Elements.Keys;
+        
+        public TBase this[string key]
+        {
+            get => this.Elements[key];
+            set => Elements[key] = value;
+        }
+
+        public TBase this[string key, TBase defaultValue]
+        {
+            get => Keys.Contains(key) ? this[key] : defaultValue;
+            set => throw new Exception("This indexer is used only for accessing elements");
+        }
+
+        public override string ToString() =>
+            string.Join(separator: "\n", values: Elements.Select(x => $"{x.Key} -> {x.Value.ToString()}"));
+
+        public IEnumerable<Tuple<String, TBase>> ElementWiseOp(VectorObject<TBase> that, Func<Tuple<TBase, TBase>, TBase> op)
+        {
+            string[] keys_1 = this.Keys.ToArray();
+            string[] keys_2 = that.Keys.ToArray();
+            if (!(keys_1.All(k => keys_2.Contains(k)) || keys_2.All(k => keys_1.Contains(k))))
+                throw new VectorExceptions.DifferentKeysException(keys_1, keys_2);
+            else return keys_1.Select(k => Tuple.Create(k, op(Tuple.Create(this[k], that[k]))));
+        }
+        
+        public IEnumerable<Tuple<String, TBase>> ElementWiseOpImputeMissingKeys(VectorObject<TBase> that, Func<Tuple<TBase, TBase>, TBase> op, TBase defaultValue)
+        {
+            List<string> mergedKeys = new List<string>(this.Keys);
+            foreach (string k in that.Keys)
+                if(!mergedKeys.Contains(k)) mergedKeys.Add(k);
+            return mergedKeys.Select(k => Tuple.Create(k, op(Tuple.Create(this[k, defaultValue], that[k, defaultValue]))));
+        }
+
+        public abstract bool EqualsTo(VectorObject<TBase> that);
+
+        public static bool operator ==(VectorObject<TBase> a, VectorObject<TBase> b) => a.EqualsTo(b);
+        public static bool operator !=(VectorObject<TBase> a, VectorObject<TBase> b) => !(a == b);
+    }
+}
