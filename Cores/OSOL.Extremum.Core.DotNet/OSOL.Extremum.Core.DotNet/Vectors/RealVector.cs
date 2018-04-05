b900be687462aa5a43cf35106364256e397bf8e1
wol4aravio <wol4aravio@yandex.ru>
2018-04-05 16:49:50 +0300

    Do not hurry

---
+++ Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/RealVector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OSOL.Extremum.Core.DotNet.Vectors
+{
+    public class RealVector
+    {
+        
+    }
+}
--- Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/VectorObject.cs
+++ Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/VectorObject.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace OSOL.Extremum.Core.DotNet.Vectors
 {
