821e62465225a4d3e28999a1966bd7cc9fd793eb
wol4aravio <wol4aravio@yandex.ru>
2018-04-05 16:15:18 +0300

    VectorObject

--- Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/VectorObject.cs
+++ Cores/OSOL.Extremum.Core.DotNet/OSOL.Extremum.Core.DotNet/Vectors/VectorObject.cs
@@ -45,8 +45,29 @@ namespace OSOL.Extremum.Core.DotNet.Vectors
         }
 
         public abstract bool EqualsTo(VectorObject<TBase> that);
-
         public static bool operator ==(VectorObject<TBase> a, VectorObject<TBase> b) => a.EqualsTo(b);
         public static bool operator !=(VectorObject<TBase> a, VectorObject<TBase> b) => !(a == b);
+
+        public abstract VectorObject<TBase> Add(VectorObject<TBase> that);
+        public static VectorObject<TBase> operator +(VectorObject<TBase> a, VectorObject<TBase> b) => a.Add(b);
+        
+        public abstract VectorObject<TBase> AddImputeMissingKeys(VectorObject<TBase> that, TBase defaultValue);
+        
+        public abstract VectorObject<TBase> Multiply(double coefficient);
+        public static VectorObject<TBase> operator *(VectorObject<TBase> a, double coefficient) => a.Multiply(coefficient);
+
+        public VectorObject<TBase> Neg() => this.Multiply(coefficient: -1.0);
+        public static VectorObject<TBase> operator -(VectorObject<TBase> a) => a.Neg();
+
+        public VectorObject<TBase> Subtract(VectorObject<TBase> that) => this.Add(that.Neg());
+        public static VectorObject<TBase> operator -(VectorObject<TBase> a, VectorObject<TBase> b) => a.Subtract(b);
+        
+        public abstract VectorObject<TBase> SubtractImputeMissingKeys(VectorObject<TBase> that, TBase defaultValue);
+        
+        public abstract VectorObject<TBase> Multiply(VectorObject<TBase> that);
+        public static VectorObject<TBase> operator *(VectorObject<TBase> a, VectorObject<TBase> b) => a.Multiply(b);
+        
+        public abstract VectorObject<TBase> MultiplyImputeMissingKeys(VectorObject<TBase> that, TBase defaultValue);
+
     }
 }
