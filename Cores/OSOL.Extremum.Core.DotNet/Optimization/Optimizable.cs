5a291b4a6cf41ee5da495e8e9695471519d642a7
wol4aravio <wol4aravio@yandex.ru>
2018-04-11 09:44:05 +0300

    Optimizable Interface

---
+++ Cores/OSOL.Extremum.Core.DotNet/Optimization/Optimizable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using OSOL.Extremum.Core.DotNet.Vectors;
+
+
+namespace OSOL.Extremum.Core.DotNet.Optimization
+{
+    public interface IOptimizable<TBase, TFuncType>
+    {
+        TBase MoveBy(Dictionary<string, double> delta);
+
+        TBase Constrain(Dictionary<string, Tuple<double, double>> area);
+
+        Double GetPerformance(Func<Dictionary<string, TFuncType>, TFuncType> f);
+
+        VectorObject<double> ToBasicForm();
+
+        string ConvertToJson();
+    }
+}
