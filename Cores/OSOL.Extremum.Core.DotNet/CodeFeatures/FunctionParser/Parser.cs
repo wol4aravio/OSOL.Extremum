f97b97fefee2b1a6c2cd912686bc25e25d18bf88
wol4aravio <wol4aravio@yandex.ru>
2018-04-16 19:42:22 +0300

    DotNet: Parser Begining

---
+++ Cores/OSOL.Extremum.Core.DotNet/CodeFeatures/FunctionParser/Parser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace OSOL.Extremum.Core.DotNet.CodeFeatures.FunctionParser
+{
+    public static class Parser
+    {
+        private static Tuple<string, string> space = Tuple.Create(" ", "");
+        private static Tuple<string, string> doubleMinus = Tuple.Create("\\-\\-", "+");
+        private static Tuple<string, string> plusMinus = Tuple.Create("\\+\\-", "-");
+        private static Tuple<string, string> minusPlus = Tuple.Create("\\-\\+", "-");
+        private static Tuple<string, string> inTheBeginning = Tuple.Create("^\\-", "~");
+        private static Tuple<string, string> afterOpeningBracket = Tuple.Create("\\(\\-", "(~");
+        private static Tuple<string, string> unnecessaryAddition = Tuple.Create("\\(\\+", "(");
+        
+        private static string tokenRegex = "(?<=[-+{*}/{)~(}^])|(?=[-+{*}/{)~(}^])";
+        
+        private static Tuple<string, string>[] rules = new[]
+        {
+            space,
+            doubleMinus,
+            plusMinus,
+            minusPlus,
+            inTheBeginning,
+            afterOpeningBracket,
+            unnecessaryAddition
+        };
+
+        public static string Prepare(string str) =>
+            rules.Aggregate(str, (f, rule) => Regex.Replace(f, rule.Item1, rule.Item2));
+
+        public static string[] ToTokens(string str) => Regex.Split(str, tokenRegex).Where(_ => _.Length > 0).ToArray();
+//
+//        def toTokens(str: String): Seq[String] =
+//        str.split(tokenRegex)
+
+    }
+}
---
+++ Cores/OSOL.Extremum.Core.DotNet/Tests/ParserTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Xunit;
+
+using OSOL.Extremum.Core.DotNet.CodeFeatures.FunctionParser;
+
+namespace OSOL.Extremum.Core.DotNet.Tests
+{
+    public static class ParserTests
+    {
+        private static string str = "-x -+ 1.0 -- (sin(y) + cos(y)) * (exp(-x) + ln(10 + -y)) / (abs(-3.0) ^ sqrt(+4.0))";
+        
+        [Fact]
+        public static void TestSplitToTokens()
+        {
+            var tokens = Parser.ToTokens(Parser.Prepare(str));
+            var desiredTokens = new[]
+            {
+                "~", "x", "-", "1.0", "+", "(", "sin", "(", "y", ")", "+", "cos", "(", "y", ")", ")", "*", "(", "exp",
+                "(", "~", "x", ")", "+", "ln", "(", "10", "-", "y", ")", ")", "/", "(", "abs", "(", "~", "3.0", ")",
+                "^", "sqrt", "(", "4.0", ")", ")"
+            };
+            Assert.True(tokens.Zip(desiredTokens, (x, y) => x.Equals(y)).All(_ => _));
+        }
+    }
+}
