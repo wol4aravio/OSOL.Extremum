53827cd1eec68adb9d590b1b524f06166b74a3c5
wol4aravio <wol4aravio@yandex.ru>
2018-04-18 11:26:52 +0300

    Scala RS: New Core Version

--- OptimizationAlgorithms/Scala/RandomSearch/build.sbt
+++ OptimizationAlgorithms/Scala/RandomSearch/build.sbt
@@ -1,6 +1,6 @@
 name := "RandomSearch"
 
-version := "0.0.1"
+version := "0.0.3"
 
 scalaVersion := "2.12.5"
 
@@ -10,4 +10,4 @@ resolvers ++= Seq(
   "sonatype snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
   "sonatype releases" at "https://oss.sonatype.org/content/repositories/releases")
 
-libraryDependencies += "com.github.wol4aravio" %% "OSOL.Extremum.ScalaCore" % "0.0.2-SNAPSHOT"
+libraryDependencies += "com.github.wol4aravio" %% "OSOL.Extremum.ScalaCore" % "0.0.3-SNAPSHOT"
--- OptimizationAlgorithms/Scala/RandomSearch/src/test/scala/OSOL/Extremum/Core/Scala/Optimization/RandomSearchSuite.scala
+++ OptimizationAlgorithms/Scala/RandomSearch/src/test/scala/OSOL/Extremum/Core/Scala/Optimization/RandomSearchSuite.scala
@@ -1,39 +1,20 @@
 package OSOL.Extremum.Core.Scala.Optimization
 
-import OSOL.Extremum.Core.Scala.Vectors.RealVector
+import OSOL.Extremum.Core.Scala.Optimization.Testing.RealTester
 import org.scalatest.FunSuite
 
 class RandomSearchSuite extends FunSuite {
 
-  val eps = 1e-3
   val r: Double = 1.0
   val fiveSec: Double = 5.0
 
-
-  def getNorm(r: RealVector): Double = math.sqrt(r.elements.values.map(v => v * v).sum)
-
-  test("Test # 1") {
-    val f1: Map[String, Double] => Double = (v: Map[String, Double]) => v("x") * v("x")
-    val a1: Area = Seq("x").map((_, (-10.0, 10.0))).toMap
-    val tool: Algorithm[RealVector, Double, RealVector] = RandomSearch.createFixedStepRandomSearch(radius = r, maxTime = a1.size * fiveSec)
-    val r1: RealVector = tool.work(f1, a1)
-    assert(getNorm(r1) <= eps)
-  }
-
-  test("Test # 2") {
-    val f2: Map[String, Double] => Double = (v: Map[String, Double]) => v("x") * v("x") + v("y") * v("y")
-    val a2: Area = Seq("x", "y").map((_, (-10.0, 10.0))).toMap
-    val tool: Algorithm[RealVector, Double, RealVector] = RandomSearch.createFixedStepRandomSearch(radius = r, maxTime = a2.size * fiveSec)
-    val r2: RealVector = tool.work(f2, a2)
-    assert(getNorm(r2) <= eps)
-  }
-
-  test("Test # 3") {
-    val f3: Map[String, Double] => Double = (v: Map[String, Double]) => v("x") * v("x") + v("y") * v("y") + v("z") * v("z")
-    val a3: Area = Seq("x", "y", "z").map((_, (-10.0, 10.0))).toMap
-    val tool: Algorithm[RealVector, Double, RealVector] = RandomSearch.createFixedStepRandomSearch(radius = r, maxTime = a3.size * fiveSec)
-    val r3: RealVector = tool.work(f3, a3)
-    assert(getNorm(r3) <= eps)
+  test("Test Random Search")
+  {
+    val tester = new RealTester
+    assert(tester(
+      RandomSearch.createFixedStepRandomSearch(radius = r, maxTime = 1 * fiveSec),
+      RandomSearch.createFixedStepRandomSearch(radius = r, maxTime = 2 * fiveSec),
+      RandomSearch.createFixedStepRandomSearch(radius = r, maxTime = 3 * fiveSec)))
   }
 
 }
