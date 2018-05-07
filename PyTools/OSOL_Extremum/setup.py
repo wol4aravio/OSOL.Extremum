adff46bf35249324f020b16b513062773b5cb75f
wol4aravio <wol4aravio@yandex.ru>
2018-05-07 22:11:26 +0300

    travis: fix 11

--- PyTools/OSOL_Extremum/setup.py
+++ PyTools/OSOL_Extremum/setup.py
@@ -34,8 +34,6 @@ setup(
                       "sympy==1.1.1",
                       "flask==0.12.2"],
 
-    # scripts=['OSOL_Extremum/computational_core/run_core.py']
-
     entry_points={
         'console_scripts': ['run_core=OSOL_Extremum.computational_core.__main__:main']
     }
