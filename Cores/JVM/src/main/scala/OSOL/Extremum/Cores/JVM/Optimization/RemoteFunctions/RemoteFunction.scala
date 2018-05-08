054fde37f2a478b29d5a35d1554d263c01086202
wol4aravio <wol4aravio@yandex.ru>
2018-05-08 23:01:03 +0300

    JVM Core: Kill by PID

--- Cores/JVM/src/main/scala/OSOL/Extremum/Cores/JVM/Optimization/RemoteFunctions/RemoteFunction.scala
+++ Cores/JVM/src/main/scala/OSOL/Extremum/Cores/JVM/Optimization/RemoteFunctions/RemoteFunction.scala
@@ -1,21 +1,32 @@
 package OSOL.Extremum.Cores.JVM.Optimization.RemoteFunctions
 
-import scala.sys.process.Process
+//import scala.sys.process.Process
 
 abstract class RemoteFunction[FuncType](val json: String, val port: Int, val field: String) {
 
-  val process_specs = Process("run_core", Seq("--core", json, "--port", port.toString))
+  val process_specs = Array("run_core", "--core", json, "--port", port.toString)
   var server_process: Process = null
+  var pid = -1
 
   def initialize(): Unit = {
-    server_process = process_specs.run()
+    server_process = Runtime.getRuntime.exec(process_specs)
+    val f = server_process.getClass.getDeclaredField("pid");
+    f.setAccessible(true)
+    pid = f.getInt(server_process)
+
     Thread.sleep(5000)
   }
 
   def apply(values: Map[String, FuncType]): FuncType
 
   def terminate(): Unit = {
-    server_process.destroy()
+    try
+      {
+        Runtime.getRuntime.exec(s"kill -9 $pid")
+      }
+    catch {
+      case _: Exception => Runtime.getRuntime.exec(s"taskkill /F /PID $pid")
+    }
     Thread.sleep(5000)
   }
 
