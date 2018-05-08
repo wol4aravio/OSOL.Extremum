edf70a704f01f09db965f33086cc7f9aef62722e
wol4aravio <wol4aravio@yandex.ru>
2018-05-09 02:06:17 +0300

    Disable

--- PyTools/OSOL_Extremum/computational_core/tools.py
+++ PyTools/OSOL_Extremum/computational_core/tools.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import logging
 
 from OSOL_Extremum.computational_core.computational_core import *
 from OSOL_Extremum.arithmetics.interval import Interval
@@ -7,6 +8,9 @@ from OSOL_Extremum.arithmetics.interval import Interval
 def create_app():
 
     app = Flask(__name__)
+    log = logging.getLogger('werkzeug')
+    log.disabled = True
+    app.logger.disabled = True
 
     @app.route('/process_request', methods=['GET'])
     def process_request():
