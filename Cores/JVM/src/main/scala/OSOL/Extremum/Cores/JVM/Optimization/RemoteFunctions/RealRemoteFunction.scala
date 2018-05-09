57e8c0b715d6424dd79138c7e73bb04fa4489cca
wol4aravio <wol4aravio@yandex.ru>
2018-05-10 01:48:08 +0300

    LocalHost to numbers

--- Cores/JVM/src/main/scala/OSOL/Extremum/Cores/JVM/Optimization/RemoteFunctions/RealRemoteFunction.scala
+++ Cores/JVM/src/main/scala/OSOL/Extremum/Cores/JVM/Optimization/RemoteFunctions/RealRemoteFunction.scala
@@ -4,7 +4,7 @@ class RealRemoteFunction(override val json: String, override val port: Int, over
   extends RemoteFunction[Double](json, port, field) {
 
   final override def apply(values: Map[String, Double]): Double = {
-    val url = s"http://localhost:${this.port}/process_request?field=${this.field}" + values.map { case (k, v) => s"&$k=$v"}.mkString("")
+    val url = s"http://127.0.0.1:${this.port}/process_request?field=${this.field}" + values.map { case (k, v) => s"&$k=$v"}.mkString("")
     val response = scala.io.Source.fromURL(url).mkString
     response.toDouble
   }
