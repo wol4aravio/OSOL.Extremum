258c0bf70fc3fbc6462986194a3ebc5455f39521
wol4aravio <wol4aravio@yandex.ru>
2018-05-09 15:36:44 +0300

    JVM Core: localhost to 127.0.0.1

--- Cores/JVM/src/main/scala/OSOL/Extremum/Cores/JVM/Optimization/RemoteFunctions/IntervalRemoteFunction.scala
+++ Cores/JVM/src/main/scala/OSOL/Extremum/Cores/JVM/Optimization/RemoteFunctions/IntervalRemoteFunction.scala
@@ -8,7 +8,7 @@ class IntervalRemoteFunction(override val json: String, override val port: Int, 
   extends RemoteFunction[Interval](json, port, field) {
 
   final override def apply(values: Map[String, Interval]): Interval = {
-    val url = s"http://localhost:${this.port}/process_request?field=${this.field}&scope=interval" + values.map { case (k, v) => s"&$k=${v.toJson}"}.mkString("")
+    val url = s"http://127.0.0.1:${this.port}/process_request?field=${this.field}&scope=interval" + values.map { case (k, v) => s"&$k=${v.toJson}"}.mkString("")
     val response = scala.io.Source.fromURL(url).mkString.parseJson
     response.convertTo[Interval]
   }
