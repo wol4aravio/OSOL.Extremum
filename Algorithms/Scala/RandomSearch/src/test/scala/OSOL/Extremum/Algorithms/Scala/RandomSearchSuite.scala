032e0f51a7cb947d017fc72c963257cc64d307fb
wol4aravio <wol4aravio@yandex.ru>
2018-05-10 15:15:32 +0300

    Try Warm Up

--- Algorithms/Scala/RandomSearch/src/test/scala/OSOL/Extremum/Algorithms/Scala/RandomSearchSuite.scala
+++ Algorithms/Scala/RandomSearch/src/test/scala/OSOL/Extremum/Algorithms/Scala/RandomSearchSuite.scala
@@ -8,6 +8,17 @@ class RandomSearchSuite extends FunSuite {
   val r: Double = 1.0
   val oneMin: Double = 60.0
 
+  test("Warm Up")
+  {
+    val tester = new RealTester
+    val result = tester(
+      RandomSearch.createFixedStepRandomSearch(radius = 1.0 * r, maxTime = 1 * oneMin),
+      RandomSearch.createFixedStepRandomSearch(radius = 0.5 * r, maxTime = 2 * oneMin),
+      RandomSearch.createFixedStepRandomSearch(radius = 0.1 * r, maxTime = 5 * oneMin))
+
+    assert(true)
+  }
+
   test("Test Random Search")
   {
     val tester = new RealTester
