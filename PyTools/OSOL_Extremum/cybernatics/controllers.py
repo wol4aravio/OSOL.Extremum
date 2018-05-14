67d6924af3e3702a3098ab92a50c726cfdd184db
wol4aravio <wol4aravio@yandex.ru>
2018-05-14 23:12:32 +0300

    PyTools: PWL Controller

--- PyTools/OSOL_Extremum/cybernatics/controllers.py
+++ PyTools/OSOL_Extremum/cybernatics/controllers.py
@@ -13,13 +13,16 @@ class PiecewiseConstantController:
         return control
 
 
+class PiecewiseLinearController:
 
+    def __init__(self, switch_points, controls):
+        self.switch_points = switch_points
+        self.controls = controls
+        time_intervals = list(zip(self.switch_points[:-1], self.switch_points[1:]))
+        control_pairs = list(zip(self.controls[:-1], self.controls[1:]))
+        self.control_intervals = list(zip(time_intervals, control_pairs))
 
-
-
-
-
-
-
-
-
+    def get_control(self, t, x):
+        ((t1, t2), (c1, c2)) = next(((t1, t2), c) for ((t1, t2), c) in self.control_intervals if t1 <= t <= t2)
+        control = ((t2 - t) * c1 + (t - t1) * c2) / (t2 - t1)
+        return control
--- PyTools/OSOL_Extremum/tests/test_controllers.py
+++ PyTools/OSOL_Extremum/tests/test_controllers.py
@@ -13,3 +13,18 @@ def test_PWC():
     assert controller.get_control(0.32, x=None) == 3
     assert controller.get_control(0.55, x=None) == 5
     assert controller.get_control(0.79, x=None) == 7
+
+
+def test_PWL():
+
+    def sim(v1, v2, tol=1e-7):
+        return np.abs(v1 - v2) < tol
+
+    switch_points = np.linspace(0.0, 1.0, 11)
+    controls = range(0, 11)
+    controller = PiecewiseLinearController(switch_points, controls)
+
+    assert sim(controller.get_control(0, x=None), 0)
+    assert sim(controller.get_control(0.32, x=None), 3.2)
+    assert sim(controller.get_control(0.55, x=None), 5.5)
+    assert sim(controller.get_control(0.79, x=None), 7.9)
