4ccedef66fb794d4554f4124ab7e5b03d73dc440
wol4aravio <wol4aravio@yandex.ru>
2018-05-14 14:26:39 +0300

    DotNet: Runner Refactor

--- Apps/DotNet/Runner/Program.cs
+++ Apps/DotNet/Runner/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using CommandLine;
+using CSharpx;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using OSOL.Extremum.Cores.DotNet.Optimization;
 using OSOL.Extremum.Cores.DotNet.Optimization.RemoteFunctions;
+using OSOL.Extremum.Cores.DotNet.Vectors;
 
 namespace OSOL.Extremum.Apps.DotNet
 {
@@ -26,6 +30,41 @@ namespace OSOL.Extremum.Apps.DotNet
             
             [Option('r', "result", Required = true, HelpText = "Where to save the result")]
             public string ResultFile { get; set; }
+            
+            [Option('o', "output", Default = "json", HelpText = "Output form")]
+            public string OutputForm { get; set; }
+        }
+
+        static RealVector RunRealVectorAlgorithm(
+            Algorithm<RealVector, double, RealVector> algorithm, 
+            RealRemoteFunction f, 
+            Dictionary<string, Tuple<double, double>> area)
+        {
+            f.Initialize();
+            var result = algorithm.Work(x => f.Calculate(x), area);
+            f.Terminate();
+            return result;
+        }
+
+        static void SaveRealVectorResult(RealVector result, Options opts)
+        {
+            if (string.Equals(opts.OutputForm, "json"))
+            {
+                using (StreamWriter sw = new StreamWriter(opts.ResultFile + ".json"))
+                using (JsonWriter writer = new JsonTextWriter(sw) {Formatting = Formatting.Indented})
+                {
+                    result.ConvertToJson().WriteTo(writer);
+                }
+            }
+
+            if (string.Equals(opts.OutputForm, "csv"))
+            {
+                using (StreamWriter sw = new StreamWriter(opts.ResultFile + ".csv"))
+                {
+                    sw.WriteLine(String.Join(",", result.Elements.Select(kvp => kvp.Key)));
+                    sw.WriteLine(String.Join(",", result.Elements.Select(kvp => kvp.Value)));
+                }
+            }
         }
 
         static void Body(Options opts)
@@ -50,15 +89,9 @@ namespace OSOL.Extremum.Apps.DotNet
                 var algorithm = Algorithms.CSharp.RandomSearch.CreateFixedStepRandomSearch(radius, maxTime);
                 
                 var f = new RealRemoteFunction(opts.TaskConfig, opts.Port, opts.Field);
-                f.Initialize();
-                var result = algorithm.Work(x => f.Calculate(x), area);
-                f.Terminate();
-
-                using (StreamWriter sw = new StreamWriter(opts.ResultFile))
-                using (JsonWriter writer = new JsonTextWriter(sw){ Formatting = Formatting.Indented })
-                {
-                    result.ConvertToJson().WriteTo(writer);
-                }
+                var result = RunRealVectorAlgorithm(algorithm, f, area);
+                
+                SaveRealVectorResult(result, opts);
                 return;
             }
 
@@ -69,15 +102,9 @@ namespace OSOL.Extremum.Apps.DotNet
                 var algorithm = Algorithms.FSharp.RandomSearch.CreateFixedStepRandomSearch(radius, maxTime);
                 
                 var f = new RealRemoteFunction(opts.TaskConfig, opts.Port, opts.Field);
-                f.Initialize();
-                var result = algorithm.Work(x => f.Calculate(x), area);
-                f.Terminate();
+                var result = RunRealVectorAlgorithm(algorithm, f, area);
 
-                using (StreamWriter sw = new StreamWriter(opts.ResultFile))
-                using (JsonWriter writer = new JsonTextWriter(sw){ Formatting = Formatting.Indented })
-                {
-                    result.ConvertToJson().WriteTo(writer);
-                }
+                SaveRealVectorResult(result, opts);
                 return;
             }
 
