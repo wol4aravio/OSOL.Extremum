be35b8ccd8e68a8aac8e7caeb86d0759442ede83
wol4aravio <wol4aravio@yandex.ru>
2018-05-15 13:53:54 +0300

    PyTools: Explicit Controller

--- PyTools/OSOL_Extremum/cybernatics/controllers.py
+++ PyTools/OSOL_Extremum/cybernatics/controllers.py
@@ -1,28 +1,91 @@
+from sympy import symbols, lambdify
+from sympy.parsing.sympy_parser import parse_expr
 import numpy as np
 
 
 class PiecewiseConstantController:
 
-    def __init__(self, switch_points, controls):
+    def __init__(self, control_name, switch_points, controls):
+        self.control_name = control_name
         self.switch_points = switch_points
         self.controls = controls
-        self.timed_control = list(zip(self.switch_points, self.controls))
+
+    def set_parameters(self, parameters):
+        param_names = ['{0}_{1}'.format(self.control_name, i) for i in range(len(self.switch_points))]
+        self.controls = list(map(lambda n: parameters[n], param_names))
+        return
 
     def get_control(self, t, x):
-        control = [c for (tau, c) in self.timed_control if tau <= t][-1]
-        return control
+        timed_control = list(zip(self.switch_points, self.controls))
+        control = [c for (tau, c) in timed_control if tau <= t][-1]
+        return self.control_name, control
 
 
 class PiecewiseLinearController:
 
-    def __init__(self, switch_points, controls):
+    def __init__(self, control_name, switch_points, controls):
+        self.control_name = control_name
         self.switch_points = switch_points
         self.controls = controls
-        time_intervals = list(zip(self.switch_points[:-1], self.switch_points[1:]))
-        control_pairs = list(zip(self.controls[:-1], self.controls[1:]))
-        self.control_intervals = list(zip(time_intervals, control_pairs))
+
+    def set_parameters(self, parameters):
+        param_names = ['{0}_{1}'.format(self.control_name, i) for i in range(len(self.switch_points))]
+        self.controls = list(map(lambda n: parameters[n], param_names))
+        return
 
     def get_control(self, t, x):
-        ((t1, t2), (c1, c2)) = next(((t1, t2), c) for ((t1, t2), c) in self.control_intervals if t1 <= t <= t2)
+        time_intervals = list(zip(self.switch_points[:-1], self.switch_points[1:]))
+        control_pairs = list(zip(self.controls[:-1], self.controls[1:]))
+        control_intervals = list(zip(time_intervals, control_pairs))
+        ((t1, t2), (c1, c2)) = next(((t1, t2), c) for ((t1, t2), c) in control_intervals if t1 <= t <= t2)
         control = ((t2 - t) * c1 + (t - t1) * c2) / (t2 - t1)
-        return control
+        return self.control_name, control
+
+
+class ExplicitController:
+
+    def __init__(self, control_name, formula, vars, param_names):
+        self.control_name = control_name
+        sym_vars = list(map(symbols, vars + param_names))
+        self.vars = vars
+        self.param_names = param_names
+        self.parameters = None
+        self._f = lambdify(sym_vars, parse_expr(formula), np)
+
+    def set_parameters(self, parameters):
+        self.parameters = list(map(lambda n: parameters[n], self.param_names))
+        return
+
+    def get_control(self, t, x):
+        args = []
+        for v in self.vars:
+            if v == 't':
+                args.append(t)
+            elif v in x:
+                args.append(x[v])
+            else:
+                raise Exception("Smth wrong with formulas")
+        control = self._f(*(args + self.parameters))
+        return self.control_name, control
+
+
+def create_controller_from_dict(data):
+    if data['type'] == 'piecewise_constant':
+        switch_points = data['switch_points']
+        if 'controls' in data:
+            controls = data['controls']
+        else:
+            controls = None
+        return PiecewiseConstantController(data['name'], switch_points, controls)
+    elif data['type'] == 'piecewise_linear':
+        switch_points = data['switch_points']
+        if 'controls' in data:
+            controls = data['controls']
+        else:
+            controls = None
+        return PiecewiseLinearController(data['name'], switch_points, controls)
+    elif data['type'] == 'explicit':
+        return ExplicitController(data['control_name'], data['formula'], data['vars'], data['param_names'])
+    else:
+        raise Exception('Unsupported Controller')
+
--- PyTools/OSOL_Extremum/tests/test_controllers.py
+++ PyTools/OSOL_Extremum/tests/test_controllers.py
@@ -1,30 +1,60 @@
 import pytest
 import numpy as np
+import random
 
 from OSOL_Extremum.cybernatics.controllers import *
 
 
-def test_PWC():
-    switch_points = np.linspace(0.0, 1.0, 11)[:-1]
-    controls = range(0, 10)
-    controller = PiecewiseConstantController(switch_points, controls)
+def sim(v1, v2, tol=1e-7):
+    return v1[0] == v2[0] and np.abs(v1[1] - v2[1]) < tol
 
-    assert controller.get_control(0, x=None) == 0
-    assert controller.get_control(0.32, x=None) == 3
-    assert controller.get_control(0.55, x=None) == 5
-    assert controller.get_control(0.79, x=None) == 7
+
+def test_PWC():
+
+    switch_points = np.linspace(0.0, 1.0, 11)[:-1]
+
+    params = [('u_{}'.format(i), i) for i in range(0, 10)] + [('k_{}'.format(i), i) for i in range(0, 10)]
+    random.shuffle(params)
+    controls = dict(params)
+
+    controller = PiecewiseConstantController('u', switch_points, None)
+    controller.set_parameters(controls)
+
+    assert sim(controller.get_control(0, x=None), ('u', 0))
+    assert sim(controller.get_control(0.32, x=None), ('u', 3))
+    assert sim(controller.get_control(0.55, x=None), ('u', 5))
+    assert sim(controller.get_control(0.79, x=None), ('u', 7))
 
 
 def test_PWL():
 
-    def sim(v1, v2, tol=1e-7):
-        return np.abs(v1 - v2) < tol
-
     switch_points = np.linspace(0.0, 1.0, 11)
-    controls = range(0, 11)
-    controller = PiecewiseLinearController(switch_points, controls)
 
-    assert sim(controller.get_control(0, x=None), 0)
-    assert sim(controller.get_control(0.32, x=None), 3.2)
-    assert sim(controller.get_control(0.55, x=None), 5.5)
-    assert sim(controller.get_control(0.79, x=None), 7.9)
+    params = [('u_{}'.format(i), i) for i in range(0, 11)] + [('k_{}'.format(i), i) for i in range(0, 11)]
+    random.shuffle(params)
+    controls = dict(params)
+
+    controller = PiecewiseLinearController('u', switch_points, None)
+    controller.set_parameters(controls)
+
+    assert sim(controller.get_control(0, x=None), ('u', 0))
+    assert sim(controller.get_control(0.32, x=None), ('u', 3.2))
+    assert sim(controller.get_control(0.55, x=None), ('u', 5.5))
+    assert sim(controller.get_control(0.79, x=None), ('u', 7.9))
+
+
+def test_Explicit():
+
+    def real(a, b, t, x):
+        return a * np.exp(t) + b * np.exp(-x)
+
+    formula = 'a * exp(t) + b * exp(-x)'
+    vars = ['t', 'x']
+    param_names = ['a', 'b']
+
+    controller = ExplicitController('u', formula, vars, param_names)
+    controller.set_parameters({'a': 1, 'b': 1, 'c': 0})
+
+    assert sim(controller.get_control(0, x={'x': 1, 'y': 77.7}), ('u', real(a=1, b=1, t=0, x=1)))
+    assert sim(controller.get_control(7.9, x={'x': 3.0, 'y': -3243.323}), ('u', real(a=1, b=1, t=7.9, x=3.0)))
+    assert sim(controller.get_control(1.1, x={'x': -1.21, 'y': 2.1}), ('u', real(a=1, b=1, t=1.1, x=-1.21)))
