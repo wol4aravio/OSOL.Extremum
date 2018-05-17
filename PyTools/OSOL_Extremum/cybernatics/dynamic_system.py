662ed13cdc8c18bfd9f6b7ea4c29776a23bccc71
wol4aravio <wol4aravio@yandex.ru>
2018-05-17 14:23:39 +0300

    PyTools: DynamicSystem

--- PyTools/OSOL_Extremum/cybernatics/dynamic_system.py
+++ PyTools/OSOL_Extremum/cybernatics/dynamic_system.py
@@ -1,113 +1,164 @@
-# from OSOL_Extremum.arithmetics.interval import *
-#
-# import numpy as np
-#
-# import sys
-# import os
-#
-# cwd = os.getcwd()
-# sys.path.insert(0, cwd)
-# sys.path.insert(0, cwd[:cwd.rindex('/')])
-# sys.path.insert(0, cwd + '/temp')
-#
-#
-# class DynamicSystem:
-#
-#     def __init__(self, f, controller, sampling_type, object_type):
-#         self.f = f
-#         self.controller = controller
-#         self.sampling_type = sampling_type
-#         if sampling_type == 'Euler':
-#             self.prolong = self.prolong_Euler
-#         elif sampling_type == 'RK4':
-#             self.prolong = self.prolong_RK4
-#         else:
-#             raise Exception('Unknown sampling_type: {}'.format(sampling_type))
-#         if object_type == 'real':
-#             self.conversion = lambda v: v
-#         elif object_type == 'interval':
-#             self.conversion = lambda v: Interval.from_value(v)
-#         else:
-#             raise Exception('Unknown object_type: {}'.format(object_type))
-#
-#     def prolong_Euler(self, t, x, u, eps):
-#         return x + self.f(t, x, u) * eps
-#
-#     def prolong_RK4(self, t, x, u, eps):
-#         f = self.f
-#         c = self.conversion
-#         k1 = f(t, x, u)
-#         k2 = f(t + c(0.5) * eps, x + k1 * c(0.5) * eps, u)
-#         k3 = f(t + c(0.5) * eps, x + k2 * c(0.5) * eps, u)
-#         k4 = f(t + eps, x + k3 * eps, u)
-#         return x + (k1 + k2 * c(2.0) + k3 * c(2.0) + k4) * (eps / c(6.0))
-#
-#     def simulate(self, x0, eps, max_steps):
-#         c = self.conversion
-#         times = [c(0.0)]
-#         states = [np.array(list(map(c, x0)))]
-#         controls = []
-#         _eps = c(eps)
-#         for step_id in range(1, max_steps + 1):
-#             t = times[-1]
-#             x = states[-1]
-#             u = self.controller(t, x)
-#             x_next = self.prolong(t, x, u, _eps)
-#             times.append(t + _eps)
-#             states.append(x_next)
-#             controls.append(u)
-#         return times, states, controls
-#
-#
-# def real(t):
-#     return np.array([
-#         1 - np.cos(t),
-#         np.sin(t),
-#         1 + t - np.sin(t) - np.cos(t)])
-#
-#
-# def f(t, x, u):
-#     x1 = x[0]
-#     x2 = x[1]
-#
-#     dx1 = np.sin(t)
-#     dx2 = np.cos(t)
-#     dx3 = x1 + x2
-#
-#     return np.array([dx1, dx2, dx3])
-#
-#
-# def controller(t, x):
-#     return np.array([])
-#
-#
-# ds = DynamicSystem(f, controller, sampling_type='RK4', object_type='real')
-#
-#
-# times, states, controls = ds.simulate(x0=np.array([0, 0, 0]), eps=0.01, max_steps=1000)
-#
-# try:
-#     states = list(map(lambda iv: np.array([i.middle_point for i in iv]), states))
-#     times = np.array(list(map(lambda i: i.middle_point, times)))
-# except Exception:
-#     times = times
-#     states = states
-#
-# real_states = []
-# for t in times:
-#     real_states.append(real(t))
-#
-# deltas = []
-# for x1, x2 in zip(states, real_states):
-#     deltas.append(np.linalg.norm(x1 - x2))
-#
-# print(np.array(deltas).max())
-#
-# #
-# # intervals = np.array([Interval(1, 2), Interval(2, 3), Interval(3, 4)]) * 2
-# # # i_sin = np.sqrt(intervals)
-# # # for i1, i2 in zip(intervals, i_sin):
-# # #     print(sqrt(i1), i2)
-# #
-# # for i in intervals:
-# #     print(i)
+from OSOL_Extremum.arithmetics.interval import *
+from OSOL_Extremum.cybernatics.controllers import create_controller_from_dict
+
+from sympy import symbols, lambdify
+from sympy.parsing.sympy_parser import parse_expr
+import numpy as np
+
+
+class DynamicSystem:
+
+    def __init__(self,
+                 sampling_type, sampling_eps, sampling_max_steps,
+                 f, state_vars, initial_conditions,
+                 controllers, control_vars, control_bounds,
+                 aux, etc_vars,
+                 integral_criterion, terminal_criterion,
+                 terminal_constraints, phase_constraints):
+        self.f = f
+        self.state_vars = state_vars
+        self.initial_conditions = initial_conditions
+        self.controllers = controllers
+        self.control_vars = control_vars
+        self.control_bounds = control_bounds
+        self.aux = aux
+        self.etc_vars = etc_vars
+        self.vars_str = ['t'] + self.state_vars + self.control_vars + self.etc_vars
+        self.sym_vars = list(map(symbols, self.vars_str))
+        self.sampling_type = sampling_type
+        self.sampling_eps = sampling_eps
+        self.sampling_max_steps = sampling_max_steps
+        if sampling_type == 'Euler':
+            self.prolong = self.prolong_Euler
+        elif sampling_type == 'RK4':
+            self.prolong = self.prolong_RK4
+        else:
+            raise Exception('Unknown sampling_type: {}'.format(sampling_type))
+        self.integral_criterion = integral_criterion
+        self.terminal_criterion = terminal_criterion
+        self.terminal_constraints = terminal_constraints
+        self.phase_constraints = phase_constraints
+
+    @classmethod
+    def from_dict(cls, data):
+        sampling_type = data['sampling_type']
+        sampling_eps = data['sampling_eps']
+        sampling_max_steps = data['sampling_max_steps']
+        state_vars = []
+        for d in data['ode']:
+            state_vars.append(d['component'])
+        control_vars = []
+        for d in data['controllers']:
+            control_vars.append(d['name'])
+        etc_vars = []
+        for d in data['auxiliary']:
+            etc_vars.append(d['component'])
+        vars_str = ['t'] + state_vars + control_vars + etc_vars
+        sym_vars = list(map(symbols, vars_str))
+        f = dict()
+        for d in data['ode']:
+            f[d['component']] = lambdify(sym_vars, parse_expr(d['equation']), np)
+        initial_conditions = dict()
+        for d in data['initial_conditions']:
+            initial_conditions[d['name']] = d['value']
+        controllers = dict()
+        for d in data['controllers']:
+            controllers[d['name']] = create_controller_from_dict(d)
+        control_bounds = dict()
+        for d in data['control_bounds']:
+            control_bounds[d['name']] = (d['min'], d['max'])
+        aux = dict()
+        counter = 0
+        for d in data['auxiliary']:
+            aux[d['component']] = lambdify(sym_vars[:(len(sym_vars) - (len(etc_vars) - counter))], parse_expr(d['equation']), np)
+            counter += 1
+        integral_criterion = lambdify(sym_vars, parse_expr(data['efficiency']['integral']), np)
+        terminal_criterion = lambdify(sym_vars, parse_expr(data['efficiency']['terminal']), np)
+        terminal_constraints = []
+        for d in data['constraints']['terminal']:
+            constraint = dict()
+            constraint['equation'] = lambdify(sym_vars, parse_expr(d['equation']), np)
+            constraint['max_error'] = d['max_error']
+            constraint['penalty'] = d['penalty']
+            constraint['norm'] = d['norm']
+            terminal_constraints.append(constraint)
+        phase_constraints = []
+        for d in data['constraints']['terminal']:
+            constraint = dict()
+            constraint['equation'] = lambdify(sym_vars, parse_expr(d['equation']), np)
+            constraint['max_error'] = d['max_error']
+            constraint['penalty'] = d['penalty']
+            constraint['norm'] = d['norm']
+            phase_constraints.append(constraint)
+        return cls(sampling_type, sampling_eps, sampling_max_steps,
+                   f, state_vars, initial_conditions,
+                   controllers, control_vars, control_bounds,
+                   aux, etc_vars,
+                   integral_criterion, terminal_criterion,
+                   terminal_constraints, phase_constraints)
+
+    def get_aux(self, t, x, u):
+        values = [t] + list(x.values()) + list(u.values())
+        aux = {}
+        for name, eq in self.aux.items():
+            _a = eq(*(values + list(aux.values())))
+            aux[name] = _a
+        return aux
+
+    def prolong_Euler(self, t, x, u, a, eps):
+        values = [t] + list(x.values()) + list(u.values()) + list(a.values())
+        new_state = {}
+        for v, eq in self.f.items():
+            new_state[v] = x[v] + eq(*values) * eps
+        return new_state
+
+    def prolong_RK4(self, t, x, u, a, eps):
+        values = [t] + list(x.values()) + list(u.values()) + list(a.values())
+        k1 = {}
+        for v, eq in self.f.items():
+            k1[v] = eq(*values)
+
+        x_new = dict([(v, x[v] + 0.5 * eps * k1[v]) for v in self.state_vars])
+        a_new = self.get_aux(t + 0.5 * eps, x_new, u)
+        values = [t] + list(x_new.values()) + list(u.values()) + list(a_new.values())
+        k2 = {}
+        for v, eq in self.f.items():
+            k2[v] = eq(*values)
+
+        x_new = dict([(v, x[v] + 0.5 * eps * k2[v]) for v in self.state_vars])
+        a_new = self.get_aux(t + 0.5 * eps, x_new, u)
+        values = [t] + list(x_new.values()) + list(u.values()) + list(a_new.values())
+        k3 = {}
+        for v, eq in self.f.items():
+            k3[v] = eq(*values)
+
+        x_new = dict([(v, x[v] + eps * k3[v]) for v in self.state_vars])
+        a_new = self.get_aux(t + eps, x_new, u)
+        values = [t] + list(x_new.values()) + list(u.values()) + list(a_new.values())
+        k4 = {}
+        for v, eq in self.f.items():
+            k4[v] = eq(*values)
+
+        x_new = dict([(v, x[v] + (k1[v] + 2.0 * k2[v] + 2.0 * k3[v] + k4[v]) * (eps / 6.0)) for v in self.state_vars])
+
+        return x_new
+
+    def simulate(self, params):
+        for v in self.control_vars:
+            self.controllers[v].set_parameters(params)
+        eps = self.sampling_eps
+        max_steps = self.sampling_max_steps
+        x0 = dict([(v, self.initial_conditions[v]) for v in self.state_vars])
+        times = [0.0]
+        states = [x0]
+        controls = []
+        for step_id in range(1, max_steps + 1):
+            t = times[-1]
+            x = states[-1]
+            u = dict([self.controllers[v].get_control(t, x) for v in self.control_vars])
+            a = self.get_aux(t, x, u)
+            x_next = self.prolong(t, x, u, a, eps)
+            times.append(t + eps)
+            states.append(x_next)
+            controls.append(u)
+        return times, states, controls
