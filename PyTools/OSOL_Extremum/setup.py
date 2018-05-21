c5a4da4d4aa27050a591e1818a0644799804c150
wol4aravio <wol4aravio@yandex.ru>
2018-05-22 00:44:40 +0300

    PyTools: Version Up

--- PyTools/OSOL_Extremum/setup.py
+++ PyTools/OSOL_Extremum/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OSOL_Extremum",
-    version="0.0.5",
+    version="0.1.0",
 
     description="Computational Center for OSOL.Extremum Project",
 
