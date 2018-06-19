24ef13c358326eb066219ffc50bc5d91659c7257
wol4aravio <wol4aravio@yandex.ru>
2018-06-19 09:10:26 +0300

    Second part of runner

--- Projects/ICM_Square/runner.py
+++ Projects/ICM_Square/runner.py
@@ -1,9 +1,14 @@
 from argparse import ArgumentParser, RawTextHelpFormatter
 from multiprocessing import Pool
 
+from OSOL_Extremum.arithmetics.interval import Interval
+
 import os
 import shutil
 import subprocess
+import json
+
+import numpy as np
 
 
 def run_process_1(args):
@@ -21,6 +26,47 @@ def run_process_1(args):
 
     subprocess.call(process)
 
+    return
+
+
+def run_process_2(args):
+    alg_config, from_folder, task, seed_folder, seed, port, to_folder = args
+    task_id = task[:-5]
+
+    process = []
+    process += ['java', '-jar', '{}/OSOL.Extremum.Apps.JVM.Runner.jar'.format(os.environ['JVM_RUNNER'])]
+    process += ['--algorithm', alg_config]
+    process += ['--task', from_folder + '/' + task]
+    process += ['--port', str(port)]
+    process += ['--field', 'sim']
+    process += ['--seed', seed_folder + '/' + seed]
+    process += ['--result', to_folder + '/' + task_id, '--output', 'json']
+    # process += ['--log-states', logs_dir]
+
+    subprocess.call(process)
+
+    return
+
+
+def convert(e, points=np.linspace(0, 1, 11)):
+    coeffs = dict([(kvp['key'], Interval.from_dict(kvp['value'])) for kvp in e['IntervalVector']['elements']])
+    u1 = lambda t: coeffs['a1'] + coeffs['b1'] * t + coeffs['c1'] * t * t
+    u2 = lambda t: coeffs['a2'] + coeffs['b2'] * t + coeffs['c2'] * t * t
+    u3 = lambda t: coeffs['a3'] + coeffs['b3'] * t + coeffs['c3'] * t * t
+    u = {'u1': u1, 'u2': u2, 'u3': u3}
+    new_coeffs = []
+    for i, p in enumerate(points):
+        for u_name, u_func in u.items():
+            new_coeffs.append({'key': '{0}_{1}'.format(u_name, i), 'value': u_func(p)})
+    new_e = e.copy()
+    new_e['IntervalVector']['elements'] = new_coeffs
+    return new_e
+
+
+def modify_seed(seed_loc, seed_file, target_loc):
+    seed = json.load(open(seed_loc + '/' + seed_file, 'r'))
+    new_seed = [convert(s) for s in seed]
+    json.dump(new_seed, open(target_loc + '/' + seed_file, 'w'))
     return
 
 
@@ -52,6 +98,14 @@ def main(args):
         process_args = [(alg_config, source_folder, t, 5017 + i, target_folder) for i, t in enumerate(task_files)]
         pool.map(run_process_1, process_args)
     elif mode == 'second':
+        seed_folder = args.seed_folder
+        seed_files = sorted(list(filter(lambda f: f.endswith('json') and ('seed' in f), os.listdir(seed_folder))))
+        for s in seed_files:
+            modify_seed(seed_folder, s, target_folder)
+
+        process_args = [(alg_config, source_folder, t, target_folder, list(filter(lambda f: t[:-5] in f, seed_files))[0], 5017 + i, target_folder) for i, t in enumerate(task_files)]
+        pool.map(run_process_2, process_args)
+
         return
     else:
         pool.close()
@@ -77,6 +131,10 @@ parser.add_argument('--alg_config',
 parser.add_argument('--source_folder',
                     type=str,
                     help='Folder with tasks')
+
+parser.add_argument('--seed_folder',
+                    type=str,
+                    help='Folder with seed tasks')
 
 parser.add_argument('--target_folder',
                     type=str,
