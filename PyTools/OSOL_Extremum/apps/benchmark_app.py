cf4117a65e0cf903b5628cfc839264169dc1ae5b
wol4aravio <wol4aravio@yandex.ru>
2018-06-25 08:25:27 +0300

    Benchmark runner (iteration 0)

---
+++ PyTools/OSOL_Extremum/apps/benchmark_app.py
@@ -0,0 +1,68 @@
+import optparse
+import os
+import shutil
+import subprocess
+
+
+def get_process_template(core, algorithm, port):
+    process = []
+
+    if core.endswith('jar'):
+        process += ['java', '-jar', core]
+    elif core.endswith('dll'):
+        process += ['dotnet', core]
+    else:
+        raise Exception('Unknown core')
+
+    process += ['--algorithm', algorithm]
+    process += ['--port', str(port)]
+    process += ['--field', 'f']
+
+    return process
+
+
+def main():
+
+    parser = optparse.OptionParser()
+    parser.add_option('-a', '--algorithm',
+                      help='Path to algorithm config',
+                      type=str)
+    parser.add_option('-c', '--core',
+                      help='Path to core',
+                      type=str)
+    parser.add_option('-t', '--tasks',
+                      help='Path to folder with tasks',
+                      type=str)
+    parser.add_option('-r', '--runs',
+                      help='Number of runs per tasks',
+                      type=int)
+    parser.add_option('-o', '--output',
+                      help='Output folders',
+                      type=str)
+    parser.add_option('-P', '--port',
+                      help='Running port',
+                      type=int,
+                      default=5017)
+
+    options, _ = parser.parse_args()
+
+    process_base = get_process_template(options.core, options.algorithm, options.port)
+
+    if os.path.exists(options.output):
+        shutil.rmtree(options.output)
+    os.makedirs(options.output)
+
+    tasks = sorted(os.listdir(options.tasks))
+    for task_id, task in enumerate(tasks):
+        print('Processing {0} ({1}/{2})'.format(task, task_id + 1, len(tasks)))
+        task_name = task[:-5]
+        for i in range(options.runs):
+            print('>>> Run {0}/{1}'.format(i + 1, options.runs))
+            p = process_base.copy()
+            p += ['--task', options.tasks + '/' + task]
+            p += ['--result', options.output + '/{0}_{1}'.format(task_name, i), '--output', 'json']
+            subprocess.call(p)
+
+
+if __name__ == '__main__':
+    main()
