bd20d58f0ada1858fd81a4ae9d4bafb301738160
wol4aravio <wol4aravio@yandex.ru>
2018-07-06 14:31:37 +0300

    Vector

--- OSOL.Extremum/Numerical_Objects/Vector.py
+++ OSOL.Extremum/Numerical_Objects/Vector.py
@@ -1,4 +1,7 @@
+from Tools.Space import constrain_point
+
 import random
+import math
 
 
 class Vector(dict):
@@ -47,3 +50,58 @@ class Vector(dict):
                     possible_components = [k]
                     max_width = w
         return random.choice(possible_components)
+
+    def __add__(self, other):
+        result = {}
+        for k in self.keys | other.keys:
+            try:
+                result[k] = self[k] + other[k]
+            except KeyError:
+                raise Exception('Can\'t sum vectors with different keys: {0} VS {1}'.format(self.keys, other.keys))
+        return Vector(result)
+
+    def __rshift__(self, delta):
+        result = {}
+        for k in self.keys:
+            result[k] = self[k] + delta.get(k, 0.0)
+        return Vector(result)
+
+    def __rmul__(self, coefficient):
+        result = {}
+        for k in self.keys:
+            result[k] = coefficient * self[k]
+        return Vector(result)
+
+    def constrain(self, min_max_values):
+        result = {}
+        for k in self.keys:
+            component = self[k]
+            target_zone = min_max_values.get(k, (-math.inf, math.inf))
+            if hasattr(component, 'constrain'):
+                result[k] = component.constrain(*target_zone)
+            else:
+                result[k] = constrain_point(result[k], *target_zone)
+        return Vector(result)
+
+    def split(self, ratios, key=None):
+        if key is None:
+            split_key = self.get_widest_component()
+        else:
+            split_key = key
+
+        target_component = self[split_key]
+        if hasattr(target_component, 'split'):
+            split_result = target_component.split(ratios)
+        else:
+            split_result = [target_component] * len(ratios)
+
+        final_result = []
+        for r in split_result:
+            copy = self.copy()
+            copy[split_key] = r
+            final_result.append(copy)
+
+        return final_result
+
+    def bisect(self, key=None):
+        return self.split(ratios=[1.0, 1.0], key=key)
