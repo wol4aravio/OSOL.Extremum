ead6fcd6aeb59cc0541108ee399ff7594cab15fc
wol4aravio <wol4aravio@yandex.ru>
2018-07-06 23:43:51 +0300

    Algorithm Template

---
+++ OSOL.Extremum/Optimization/Algorithm.py
@@ -0,0 +1,43 @@
+from abc import ABCMeta, abstractmethod
+
+import os
+import shutil
+import json
+
+
+class Algorithm:
+    __metaclass__ = ABCMeta
+
+    @property
+    @abstractmethod
+    def current_state(self):
+        '''Returns current state as a dictionary'''
+
+    @property
+    @abstractmethod
+    def iterations(self):
+        '''Returns list of iterations'''
+
+    @abstractmethod
+    def initialize(self, seed):
+        '''Initializes algorithm with the given seed'''
+
+    def work(self, f, area, terminator, seed=None, log_states=None):
+        self.initialize(seed)
+        if log_states is not None:
+            log_counter = 1
+            if os.path.exists(log_states):
+                shutil.rmtree(log_states)
+            os.makedirs(log_states)
+        current_iteration = self.iterations[0]
+
+        while not terminator(self.current_state):
+            current_iteration = current_iteration(f, area, self.current_state)
+            if current_iteration is None:
+                break
+            if log_states is not None:
+                json.dump(self.current_state, open(os.path.join(log_states, '{0:07d}.json'), 'w'), indent=2)
+                log_counter += 1
+
+        return self.current_state['result']
+
