5c4882987465a89a66868f92ac11d21393f68b18
wol4aravio <wol4aravio@yandex.ru>
2018-07-08 14:13:30 +0300

    better optimization

--- OSOL.Extremum/tests/test_algorithm.py
+++ OSOL.Extremum/tests/test_algorithm.py
@@ -7,26 +7,24 @@ import numpy as np
 
 
 class Dummy_Optimization(Algorithm):
-    def __init__(self, ratio):
-        self.x = None
+    def __init__(self, N, ratio):
+        self.best = None
+        self.points = None
         self.delta_area = None
         self.ratio = ratio
+        self.N = N
 
     @property
     def current_state(self):
-        return {'result': self.x}
+        return {'result': self.best}
 
     @property
     def iterations(self):
         return [self.generate_new_points]
 
     def initialize(self, f, area, seed):
-        if seed is not None:
-            if type(seed) == list:
-                self.x = min(seed, key=lambda v: f(v))
-            else:
-                self.x = seed
-        else:
+        self.points = []
+        for i in range(self.N):
             point = {}
             for k, (left, right) in area.items():
                 if np.random.uniform(-1.0, 1.0) < 0.0:
@@ -34,7 +32,9 @@ class Dummy_Optimization(Algorithm):
                 else:
                     [p1, p2] = sorted(np.random.uniform(left, right, (1, 2))[0])
                     point[k] = Interval.create_valid_interval(lower_bound=p1, upper_bound=p2)
-            self.x = Vector(point)
+            self.points.append(Vector(point))
+        self.points = sorted(self.points, key=lambda x: f(x))
+        self.best = self.points[0]
 
         self.delta_area = {}
         for k, (left, right) in area.items():
@@ -42,19 +42,18 @@ class Dummy_Optimization(Algorithm):
             self.delta_area[k] = (-shift, shift)
 
     def generate_new_points(self, f, area):
-        x = self.x
-        delta = {k: np.random.uniform(left, right) for k, (left, right) in self.delta_area.items()}
-        x_new = x >> delta
-        x_best = min(x, x_new, key=lambda v: f(v))
-        [x, x_new] = x_best.bisect()
-        x_best = min(x, x_new, key=lambda v: f(v))
-        self.x = x_best
+        new_points = []
+        for x in self.points[1:]:
+            delta = {k: np.random.uniform(left, right) for k, (left, right) in self.delta_area.items()}
+            new_points += (x >> delta).bisect()
+        self.points = sorted(new_points + self.best.bisect(), key=lambda v: f(v))[:self.N]
+        self.best = self.points[0]
         return self.generate_new_points
 
 
-a1 = Dummy_Optimization(0.01)
-a2 = Dummy_Optimization(0.005)
-a3 = Dummy_Optimization(0.001)
+a1 = Dummy_Optimization(5, 0.01)
+a2 = Dummy_Optimization(10, 0.005)
+a3 = Dummy_Optimization(25, 0.001)
 
 tester = Verifier()
 
