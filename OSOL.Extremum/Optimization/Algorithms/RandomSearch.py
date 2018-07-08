a3c023c29caf1dc74be36d84e7dde619585d0918
wol4aravio <wol4aravio@yandex.ru>
2018-07-08 19:48:46 +0300

    fix + refactor

--- OSOL.Extremum/Optimization/Algorithms/RandomSearch.py
+++ OSOL.Extremum/Optimization/Algorithms/RandomSearch.py
@@ -11,7 +11,7 @@ class RandomSearch(Algorithm):
         normally_distributed = Vector({k: v for k, v in zip(area.keys(), np.random.normal(0.0, 1.0, len(area)))})
         length = np.sqrt(sum(np.array(normally_distributed.values) ** 2))
         shift = (np.random.uniform(0.0, radius) / length) * normally_distributed
-        return (current_point >> shift.reduce_to_dict()).constrain(area)
+        return (current_point + shift).constrain(area)
 
     def __init__(self, radius):
         self._x = None
@@ -30,19 +30,27 @@ class RandomSearch(Algorithm):
 
     @property
     def current_state(self):
-        return {'result': self._x}
+        return {'result': self._x, 'efficiency': self._f_x}
 
     @property
     def iterations(self):
         return [self.generate_new_points]
 
     def initialize(self, f, area, seed):
-        point = {}
-        for k, (left, right) in area.items():
-            point[k] = np.random.uniform(left, right)
-        point = Vector(point)
-        self._x = point
-        self._f_x = f(point)
+        if seed is None:
+            point = {}
+            for k, (left, right) in area.items():
+                point[k] = np.random.uniform(left, right)
+            point = Vector(point)
+            self._x = point
+            self._f_x = f(point)
+        else:
+            if type(seed) == list:
+                self._x = sorted(seed, key=lambda v: f(v))[0]
+                self._f_x = f(self._x)
+            else:
+                self._x = seed
+                self._f_x = f(self._x)
 
     def generate_new_points(self, f, area):
         x = self._x
