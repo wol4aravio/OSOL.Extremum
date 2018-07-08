27be44d7dc587d8446f116b3eda90862fd8b6ecc
wol4aravio <wol4aravio@yandex.ru>
2018-07-08 16:30:23 +0300

    name fix

---
+++ OSOL.Extremum/tests/test_algorithms.py
@@ -0,0 +1,10 @@
+from Optimization.Algorithms.RandomSearch import RandomSearch
+from Optimization.Verifier import Verifier
+
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+
+def test_random_search():
