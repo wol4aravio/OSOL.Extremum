2de7998e691879ff56c2221b2f10e30ab5dd8f8a
wol4aravio <wol4aravio@yandex.ru>
2018-07-08 20:05:12 +0300

    IES

--- OSOL.Extremum/Tools/OptimizationTools.py
+++ OSOL.Extremum/Tools/OptimizationTools.py
@@ -1,8 +1,11 @@
 from Optimization.Algorithms.RandomSearch import RandomSearch
+from Optimization.Algorithms.IntervalExplosionSearch import IntervalExplosionSearch
 
 
 def create_algorithm_from_json(json_data):
     if 'RandomSearch' in json_data:
         return RandomSearch.from_json(json_data)
+    elif 'IntervalExplosionSearch' in json_data:
+        return IntervalExplosionSearch.from_json(json_data)
     else:
         raise Exception('Unsupported Optimization Algorithm')
--- OSOL.Extremum/tests/test_algorithms.py
+++ OSOL.Extremum/tests/test_algorithms.py
@@ -34,3 +34,35 @@ def test_random_search():
     logger = logging.getLogger('Random Search')
     assert verifier.verify([rs_1, rs_2, rs_3], logger)
     print('Done!\n')
+
+
+ies_1_config = {
+    'IntervalExplosionSearch': {
+        'max_bombs': 10,
+        'max_radius_ratio': 0.1
+    }
+}
+
+ies_2_config = {
+    'IntervalExplosionSearch': {
+        'max_bombs': 25,
+        'max_radius_ratio': 0.25
+    }
+}
+
+ies_3_config = {
+    'IntervalExplosionSearch': {
+        'max_bombs': 10,
+        'max_radius_ratio': 0.01
+    }
+}
+
+ies_1 = create_algorithm_from_json(ies_1_config)
+ies_2 = create_algorithm_from_json(ies_2_config)
+ies_3 = create_algorithm_from_json(ies_3_config)
+
+
+def test_interval_explosion_search():
+    logger = logging.getLogger('Interval Explosion Search')
+    assert verifier.verify([ies_1, ies_2, ies_3], logger)
+    print('Done!\n')
