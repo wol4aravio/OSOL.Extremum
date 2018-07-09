24b34ab2d3cc0137cbce60184171ef7d3f866adb
wol4aravio <wol4aravio@yandex.ru>
2018-07-09 16:09:09 +0300

    task + optimize script

---
+++ OSOL.Extremum/apps/optimize.py
@@ -0,0 +1,41 @@
+from Tools.OptimizationTools import *
+from Tools.Encoders import CustomEncoder
+
+from Optimization.Terminators.MaxTimeTerminator import MaxTimeTerminator
+
+import optparse
+import json
+import os
+
+
+def main():
+
+    parser = optparse.OptionParser()
+    parser.add_option('-A', '--algorithm',
+                      help='Path to algorithm config',
+                      type=str)
+    parser.add_option('-T', '--time',
+                      help='Algorithm working time',
+                      type=str)
+    parser.add_option('-P', '--problem',
+                      help='Path to task',
+                      type=str)
+    parser.add_option('-O', '--output',
+                      help='Output file path',
+                      type=str)
+
+    options, _ = parser.parse_args()
+
+    output_file = options.output
+    task = create_task_from_json(json.load(open(options.task, 'r')))
+    algorithm = create_algorithm_from_json(json.load(open(options.algorithm, 'r')))
+    mt = MaxTimeTerminator(options.working_time)
+
+    print('Processing')
+    x = algorithm.work(task['f'], task['area'], mt)
+    json.dump(x, open('{}.json'.format(output_file), 'r'), cls=CustomEncoder, indent=2)
+    print('Done\n')
+
+
+if __name__ == '__main__':
+    main()
