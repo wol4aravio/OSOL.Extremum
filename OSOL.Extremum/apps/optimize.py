a8bdc33972646cdd0bc585008f4800b2b1f08708
wol4aravio <wol4aravio@yandex.ru>
2018-07-14 19:40:35 +0300

    Seed value

--- OSOL.Extremum/apps/optimize.py
+++ OSOL.Extremum/apps/optimize.py
@@ -5,6 +5,7 @@ sys.path.append(os.getcwd())
 from Tools.OptimizationTools import *
 from Apps.tools import *
 from Tools.Encoders import CustomEncoder
+from Numerical_Objects.Vector import Vector
 
 from Optimization.Terminators.MaxTimeTerminator import MaxTimeTerminator
 
@@ -17,6 +18,10 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option('-A', '--algorithm',
                       help='Path to algorithm config',
+                      type=str)
+    parser.add_option('-S', '--seed',
+                      help='Path to seed values',
+                      default=None,
                       type=str)
     parser.add_option('-T', '--time',
                       help='Algorithm working time',
@@ -37,10 +42,16 @@ def main():
     output_file = options.output
     task = create_task_from_json(json.load(open(options.problem, 'r')))
     algorithm = create_algorithm_from_json(json.load(open(options.algorithm, 'r')))
+    seed_values = options.seed
     mt = MaxTimeTerminator(options.time)
 
     print('Processing')
-    x = algorithm.work(task['f'], task['area'], mt)
+    if seed_values is None:
+        x = algorithm.work(task['f'], task['area'], mt)
+    else:
+        seed_values = [Vector.from_json(open(f, 'r')) for f in seed_values.split(',')]
+        x = algorithm.work(task['f'], task['area'], mt, seed_values)
+
     if options.reduce:
         x = Vector(x.reduce_to_dict())
     json.dump(x, open(output_file, 'w'), cls=CustomEncoder, indent=2)
