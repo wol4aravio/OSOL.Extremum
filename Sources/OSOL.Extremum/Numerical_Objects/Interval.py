65aae40c29703c9e035b831c27c5b44d996f869a
wol4aravio <wol4aravio@yandex.ru>
2018-08-14 09:58:33 +0300

    Numerical Objects

---
+++ Sources/OSOL.Extremum/Numerical_Objects/Interval.py
@@ -0,0 +1,265 @@
+from Tools.etc import constrain_point
+
+from configparser import ConfigParser
+import warnings
+
+import math
+import json
+import os
+
+
+config = ConfigParser()
+if os.getcwd().endswith('tests') or os.getcwd().endswith('Applications'):
+    config.read('../Numerical_Objects/config.ini')
+else:
+    config.read('Numerical_Objects/config.ini')
+
+
+class Interval:
+
+    try:
+        __MIN_WIDTH = float(config.get('interval', 'min_width'))
+    except Exception:
+        warnings.warn('Can not parse config.ini file. Setting default __MIN_WIDTH = 1e-7')
+        __MIN_WIDTH = 1e-7
+
+    def __init__(self, lower_bound, upper_bound):
+        self._lower_bound = lower_bound
+        self._upper_bound = upper_bound
+
+    def __str__(self):
+        return '[{0}; {1}]'.format(self.left, self.right)
+
+    @classmethod
+    def from_value(cls, value):
+        return cls(value, value)
+
+    @classmethod
+    def from_dict(cls, dict_data):
+        return cls(**dict_data)
+
+    @classmethod
+    def from_json(cls, json_data):
+        return cls(**json_data['Interval'])
+
+    @staticmethod
+    def create_valid_interval(lower_bound, upper_bound):
+        if (upper_bound - lower_bound) < Interval.__MIN_WIDTH:
+            return 0.5 * (lower_bound + lower_bound)
+        else:
+            return Interval(lower_bound, upper_bound)
+        
+    @property
+    def left(self):
+        return self._lower_bound
+
+    @property
+    def right(self):
+        return self._upper_bound
+    
+    @property
+    def middle_point(self):
+        return 0.5 * (self.left + self.right)
+
+    @property
+    def width(self):
+        return self.right - self.left
+
+    @property
+    def radius(self):
+        return self.width / 2.0
+
+    def approximately_equals_to(self, other, max_error=1e-5):
+        def get_difference(a, b):
+            delta = math.fabs(a - b)
+            if math.isnan(delta):
+                if a == b:
+                    return 0.0
+                else:
+                    return 1.0
+            else:
+                return delta
+        error_left = get_difference(self.left, other.left)
+        error_right = get_difference(self.right, other.right)
+        return error_left + error_right <= max_error
+
+    def __eq__(self, other):
+        if isinstance(other, Interval):
+            return self.left == other.left and self.right == other.right
+        else:
+            return self.left == other and self.right == other
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __lt__(self, other):
+        if isinstance(other, Interval):
+            return self.left < other.left
+        else:
+            return self.left < other
+
+    def __le__(self, other):
+        if isinstance(other, Interval):
+            return self.left <= other.left
+        else:
+            return self.left <= other
+
+    def __gt__(self, other):
+        if isinstance(other, Interval):
+            return self.left > other.left
+        else:
+            return self.left > other
+
+    def __ge__(self, other):
+        if isinstance(other, Interval):
+            return self.left >= other.left
+        else:
+            return self.left >= other
+
+    def __neg__(self):
+        return Interval.create_valid_interval(-self.right, -self.left)
+
+    def __add__(self, other):
+        if isinstance(other, Interval):
+            return Interval.create_valid_interval(self.left + other.left, self.right + other.right)
+        else:
+            return self + Interval.from_value(other)
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        if isinstance(other, Interval):
+            return Interval.create_valid_interval(self.left - other.right, self.right - other.left)
+        else:
+            return self - Interval.from_value(other)
+
+    def __rsub__(self, other):
+        return Interval.from_value(other) - self
+
+    def __mul__(self, other):
+        if isinstance(other, Interval):
+            products = [
+                self.left * other.left,
+                self.left * other.right,
+                self.right * other.left,
+                self.right * other.right
+            ]
+            return Interval.create_valid_interval(min(products), max(products))
+        else:
+            return self * Interval.from_value(other)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        if isinstance(other, Interval):
+            if other.left > 0 or other.right < 0:
+                return self * Interval.create_valid_interval(1.0 / other.right, 1.0 / other.left)
+            elif other.left == 0.0:
+                return self * Interval.create_valid_interval(1.0 / other.right, math.inf)
+            elif other.right == 0.0:
+                return self * Interval.create_valid_interval(-math.inf, 1.0 / other.left)
+            else:
+                return Interval.create_valid_interval(-math.inf, math.inf)
+        else:
+            return self / Interval.from_value(other)
+
+    def __rtruediv__(self, other):
+        return Interval.from_value(other) / self
+
+    def __pow__(self, power, modulo=None):
+        values = [math.pow(self.left, power), math.pow(self.right, power)]
+        if power % 2 == 0 and self.left * self.right < 0:
+            values.append(0.0)
+        values = sorted(values)
+        return Interval.create_valid_interval(values[0], values[-1])
+
+    def abs(self):
+        values = [math.fabs(self.left), math.fabs(self.right)]
+        if self.left * self.right < 0:
+            values.append(0.0)
+        values = sorted(values)
+        return Interval.create_valid_interval(values[0], values[-1])
+
+    def __abs__(self):
+        return self.abs()
+
+    def sin(self):
+        if self.width > 2.0 * math.pi:
+            return Interval(-1.0, 1.0)
+        else:
+            c = 0.5 * math.pi
+            left_bound = int(math.ceil(self.left / c))
+            right_bound = int(math.floor(self.right / c))
+            points = [self.left, self.right] + list(map(lambda v: c * v, range(left_bound, right_bound + 1)))
+            mapped_points = sorted(map(math.sin, points))
+            return Interval.create_valid_interval(mapped_points[0], mapped_points[-1])
+
+    def cos(self):
+        if self.width > 2.0 * math.pi:
+            return Interval(-1.0, 1.0)
+        else:
+            c = 0.5 * math.pi
+            left_bound = int(math.ceil(self.left / c))
+            right_bound = int(math.floor(self.right / c))
+            points = [self.left, self.right] + list(map(lambda v: c * v, range(left_bound, right_bound + 1)))
+            mapped_points = sorted(map(math.cos, points))
+            return Interval.create_valid_interval(mapped_points[0], mapped_points[-1])
+
+    def exp(self):
+        return Interval.create_valid_interval(math.exp(self.left), math.exp(self.right))
+
+    def sqrt(self):
+        if self.right < 0.0:
+            raise(Exception('Can\'t perform operation to pure negative interval'))
+        else:
+            if self.left < 0.0:
+                return Interval.create_valid_interval(0.0, math.sqrt(self.right))
+            else:
+                return Interval.create_valid_interval(math.sqrt(self.left), math.sqrt(self.right))
+
+    def log(self):
+        if self.right <= 0.0:
+            raise(Exception('Can\'t perform operation to pure negative interval'))
+        else:
+            if self.left <= 0.0:
+                return Interval.create_valid_interval(-math.inf, math.log(self.right))
+            else:
+                return Interval.create_valid_interval(math.log(self.left), math.log(self.right))
+
+    def constrain(self, min_value, max_value):
+        return Interval.create_valid_interval(constrain_point(self.left, min_value, max_value),
+                                              constrain_point(self.right, min_value, max_value))
+
+    def split(self, ratios):
+        ratio_sum = sum(ratios)
+        w = self.width
+        intervals = []
+        for i in range(len(ratios)):
+            intervals.append(Interval.create_valid_interval(lower_bound=self.left + sum(ratios[:i]) * w / ratio_sum,
+                                                            upper_bound=self.left + sum(ratios[:(i + 1)]) * w / ratio_sum))
+        return intervals
+
+    def bisect(self):
+        return self.split([1.0, 1.0])
+
+
+def sin(i):
+    return i.sin()
+
+
+def cos(i):
+    return i.cos()
+
+
+def exp(i):
+    return i.exp()
+
+
+def sqrt(i):
+    return i.sqrt()
+
+
+def log(i):
+    return i.log()
---
+++ Sources/OSOL.Extremum/Numerical_Objects/Vector.py
@@ -0,0 +1,168 @@
+from Tools.etc import constrain_point
+from Numerical_Objects.Interval import Interval
+
+import random
+import math
+import json
+
+
+class Vector:
+
+    def __init__(self, values):
+        self._values = values
+
+    def __str__(self):
+        return ' x '.join(['({0}: {1})'.format(k, v) for k, v in self._values.items()])
+
+    def __getitem__(self, var_name):
+        return self._values[var_name]
+
+    def __setitem__(self, key, value):
+        self._values[key] = value
+
+    @classmethod
+    def from_dict(cls, dict_data):
+        return cls(**dict_data)
+
+    @classmethod
+    def from_json(cls, json_data):
+        values_dict = json_data['Vector']['values']
+        for k, v in values_dict.items():
+            if isinstance(v, dict):
+                if 'Interval' in v:
+                    values_dict[k] = Interval.from_dict(v['Interval'])
+                else:
+                    raise Exception('Can\'t deserialize object: {}'.format(v))
+        return cls(values_dict)
+
+    def copy(self):
+        return Vector(self._values.copy())
+
+    @property
+    def keys(self):
+        return set(self._values.keys())
+
+    @property
+    def values(self):
+        return list(self._values.values())
+
+    @property
+    def length(self):
+        s = 0
+        for v in self.values:
+            s += v ** 2
+        if isinstance(s, Interval):
+            return Interval.sqrt(s)
+        else:
+            return math.sqrt(s)
+
+    @property
+    def dim(self):
+        return len(self._values)
+
+    def __eq__(self, other):
+        if self.keys != other.keys:
+            return False
+        else:
+            for k in (self.keys | other.keys):
+                if self[k] != other[k]:
+                    return False
+        return True
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def get_widest_component(self):
+        def get_width(v):
+            if hasattr(v, 'width'):
+                return v.width
+            else:
+                return 0.0
+        max_width = 0.0
+        possible_components = []
+        for k in self.keys:
+            w = get_width(self[k])
+            if w == max_width:
+                possible_components.append(k)
+            else:
+                if w > max_width:
+                    possible_components = [k]
+                    max_width = w
+        return random.choice(possible_components)
+
+    def __add__(self, other):
+        result = {}
+        for k in self.keys | other.keys:
+            try:
+                result[k] = self[k] + other[k]
+            except KeyError:
+                raise Exception('Can\'t sum vectors with different keys: {0} VS {1}'.format(self.keys, other.keys))
+        return Vector(result)
+
+    def __rshift__(self, delta):
+        result = {}
+        for k in self.keys:
+            result[k] = self[k] + delta.get(k, 0.0)
+        return Vector(result)
+
+    def __mul__(self, coefficient):
+        result = {}
+        for k in self.keys:
+            result[k] = coefficient * self[k]
+        return Vector(result)
+
+    def __rmul__(self, coefficient):
+        return self.__mul__(coefficient)
+
+    def __neg__(self):
+        return self * (-1.0)
+
+    def __sub__(self, other):
+        return self + other.__neg__()
+
+    def constrain(self, min_max_values):
+        result = {}
+        for k in self.keys:
+            component = self[k]
+            target_zone = min_max_values.get(k, (-math.inf, math.inf))
+            if hasattr(component, 'constrain'):
+                result[k] = component.constrain(*target_zone)
+            else:
+                result[k] = constrain_point(self[k], *target_zone)
+        return Vector(result)
+
+    def split(self, ratios, key=None):
+        if key is None:
+            split_key = self.get_widest_component()
+        else:
+            split_key = key
+
+        target_component = self[split_key]
+        if hasattr(target_component, 'split'):
+            split_result = target_component.split(ratios)
+        else:
+            split_result = [target_component] * len(ratios)
+
+        final_result = []
+        for r in split_result:
+            copy = self.copy()
+            copy[split_key] = r
+            final_result.append(copy)
+
+        return final_result
+
+    def bisect(self, key=None):
+        return self.split(ratios=[1.0, 1.0], key=key)
+
+    def reduce_to_dict(self, point_reduction=True):
+        result = {}
+        for k in self.keys:
+            v = self[k]
+            if isinstance(v, Interval):
+                if point_reduction:
+                    result[k] = v.middle_point
+                else:
+                    result[k] = v
+            else:
+                result[k] = float(v)
+        return result
