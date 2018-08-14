b379cc9ee4f3e05da28f41fe072f7ef46e799090
wol4aravio <wol4aravio@yandex.ru>
2018-08-14 10:00:05 +0300

    Tests

---
+++ Sources/OSOL.Extremum/tests/test_modified_hybrid_memetic_algorithm.py
@@ -0,0 +1,81 @@
+from Tools.OptimizationTools import *
+from Optimization.Verifier import Verifier
+
+import os
+import shutil
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+verifier = Verifier()
+
+rs = {
+    'RandomSearch': {
+        'radius': 0.1
+    }
+}
+mt = {
+    'MaxTimeTerminator': {
+        'max_time': 's:0.1'
+    }
+}
+
+mhma_1_config = {
+    'ModifiedHybridMemeticAlgorithm': {
+        'population_size': 10,
+        'distance_bias': 0.1,
+        'pool_size': 5,
+        'pool_purge_size': 5,
+        'combination_algorithm': rs,
+        'combination_terminator': mt,
+        'path_relinking_parameter': 5,
+        'local_improvement_parameter': 5,
+        'delta_path_relinking': 5,
+        'delta_local_improvement': 0.01
+    }
+}
+mhma_2_config = {
+    'ModifiedHybridMemeticAlgorithm': {
+        'population_size': 25,
+        'distance_bias': 0.01,
+        'pool_size': 3,
+        'pool_purge_size': 2,
+        'combination_algorithm': rs,
+        'combination_terminator': mt,
+        'path_relinking_parameter': 3,
+        'local_improvement_parameter': 3,
+        'delta_path_relinking': 3,
+        'delta_local_improvement': 0.01
+    }
+}
+mhma_3_config = {
+    'ModifiedHybridMemeticAlgorithm': {
+        'population_size': 5,
+        'distance_bias': 0.1,
+        'pool_size': 3,
+        'pool_purge_size': 1,
+        'combination_algorithm': rs,
+        'combination_terminator': mt,
+        'path_relinking_parameter': 10,
+        'local_improvement_parameter': 10,
+        'delta_path_relinking': 10,
+        'delta_local_improvement': 0.05
+    }
+}
+
+mhma_1 = create_algorithm_from_json(mhma_1_config)
+mhma_2 = create_algorithm_from_json(mhma_2_config)
+mhma_3 = create_algorithm_from_json(mhma_3_config)
+
+
+def test_logging():
+    log_dir = 'log_test_mhma'
+    mhma_1.work(verifier._test_functions[0], verifier._search_area[0], verifier._mt, log_states=log_dir)
+    assert len(os.listdir(log_dir)) > 0
+    shutil.rmtree(log_dir)
+
+
+def test_algorithm():
+    logger = logging.getLogger('Modified Hybrid Memetic Algorithm')
+    assert verifier.verify([mhma_1, mhma_2, mhma_3], logger)
+    print('Done!\n')
---
+++ Sources/OSOL.Extremum/tests/test_unconstrained_optimization.py
@@ -0,0 +1,46 @@
+from Tools.Encoders import CustomEncoder
+from Optimization.Tasks.UnconstrainedOptimization import UnconstrainedOptimization
+from Numerical_Objects.Interval import Interval as I
+from Numerical_Objects.Vector import Vector as V
+
+from math import fabs
+import json
+
+
+task = {
+    'f': '1 * x ** 2 + 2 * y ** 2 + 3 * z ** 2',
+    'variables': ['x', 'y', 'z']
+}
+
+tol = 1e-7
+
+
+def almost_equal_real(v1, v2):
+    return fabs(v1 - v2) < tol
+
+
+def almost_equal_interval(i1, i2):
+    return fabs(i1.left - i2.left) + fabs(i1.right - i2.right) < 2 * tol
+
+
+def test_from_json():
+    f = UnconstrainedOptimization.from_dict(task)
+    assert f._f == UnconstrainedOptimization.from_json(json.loads(json.dumps(f, cls=CustomEncoder)))._f
+    assert f._variables == UnconstrainedOptimization.from_json(json.loads(json.dumps(f, cls=CustomEncoder)))._variables
+
+
+def test_real_calculation():
+    f = UnconstrainedOptimization.from_dict(task)
+    assert almost_equal_real(f({'x': 1, 'y': 2, 'z': 3}), 36.0)
+    assert almost_equal_real(f({'x': -1, 'y': -2, 'z': -3}), 36.0)
+    assert almost_equal_real(f({'x': 2, 'y': 2, 'z': 2}), 24.0)
+
+
+def test_interval_calculation():
+    f = UnconstrainedOptimization.from_dict(task)
+    assert almost_equal_interval(f({'x': I(1, 2), 'y': I(2, 3), 'z': I(3, 4)}), I(36.0, 70.0))
+
+
+def test_mixed_calculation():
+    f = UnconstrainedOptimization.from_dict(task)
+    assert almost_equal_interval(f(V({'x': I(1, 2), 'y': 2, 'z': 3})), I(36.0, 39.0))
