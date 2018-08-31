ed02dfc58be06d1a7c53cddb9db0db825b953de0
wol4aravio <wol4aravio@yandex.ru>
2018-08-31 11:25:06 +0300

    DS: search phase

--- OSOL/Extremum/Optimization/Algorithms/DolphinSwarm.py
+++ OSOL/Extremum/Optimization/Algorithms/DolphinSwarm.py
@@ -7,16 +7,25 @@ import numpy as np
 
 class DolphinSwarm(Algorithm):
 
-    def __init__(self, number_of_dolphins):
+    def __init__(self, number_of_dolphins, speed, search_time, number_of_directions):
         self._dolphins = None
         self._dolphins_K = None
+        self._dolphins_L = None
         self._dolphins_K_fit = None
+        self._dolphins_L_fit = None
 
         self._number_of_dolphins = number_of_dolphins
+        self._speed = speed
+        self._search_time = search_time
+        self._number_of_directions = number_of_directions
 
     @classmethod
     def from_dict(cls, dict_data):
-        return cls(dict_data['number_of_dolphins'])
+        return cls(
+            dict_data['number_of_dolphins'],
+            dict_data['speed'],
+            dict_data['search_time'],
+            dict_data['number_of_directions'])
 
     @classmethod
     def from_json(cls, json_data):
@@ -24,7 +33,10 @@ class DolphinSwarm(Algorithm):
 
     def to_dict(self):
         return {
-            'number_of_dolphins': self._number_of_dolphins
+            'number_of_dolphins': self._number_of_dolphins,
+            'speed': self._speed,
+            'search_time': self._search_time,
+            'number_of_directions': self._number_of_directions
         }
 
     def to_json(self):
@@ -34,9 +46,9 @@ class DolphinSwarm(Algorithm):
     def current_state(self):
         return {'result': self._refresh_best_dolphin(), 'dolphins_K': self._dolphins_K}
 
-    # @property
-    # def iterations(self):
-    #     return [self.generate_new_point]
+    @property
+    def iterations(self):
+        return [self.search_phase]
 
     def _refresh_best_dolphin(self):
         min_id = np.argmin(self._dolphins_K_fit)
@@ -59,7 +71,19 @@ class DolphinSwarm(Algorithm):
         self._dolphins_K_fit = [f(d) for d in self._dolphins_K]
         return
 
-
-
-    # def search_phase(self, f, area):
+    def search_phase(self, f, area):
+        self._dolphins_L = []
+        self._dolphins_L_fit = []
+        for d_id, d in enumerate(self._dolphins):
+            velocities = [generate_random_point_in_rectangular({k: (-1.0, 1.0) for k in area.keys})
+                          for _ in range(self._number_of_directions)]
+            investigated_locations = [d + v * i for v in velocities for i in range(1, self._search_time + 1)]
+            fitness = [f(loc) for loc in investigated_locations]
+            best_loc_id = np.argmin(fitness)
+            self._dolphins_L.append(investigated_locations[best_loc_id])
+            self._dolphins_L_fit.append(fitness[best_loc_id])
+            if self._dolphins_L_fit[d_id] < self._dolphins_K_fit[d_id]:
+                self._dolphins_K_fit[d_id] = self._dolphins_L_fit[d_id]
+                self._dolphins_K = self._dolphins_L[d_id].copy()
+        return
 
