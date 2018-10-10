9b7fd95e1650eaee262ca5cad5848b3f6f7efe40
wol4aravio <wol4aravio@yandex.ru>
2018-10-10 17:25:48 +0300

    RS

--- OSOL/Extremum/Optimization/Algorithms/RandomSearch.py
+++ OSOL/Extremum/Optimization/Algorithms/RandomSearch.py
@@ -2,8 +2,6 @@ from OSOL.Extremum.Optimization.Algorithms.Algorithm import Algorithm
 from OSOL.Extremum.Optimization.Algorithms.tools import *
 from OSOL.Extremum.Numerical_Objects.Vector import Vector
 
-import numpy as np
-
 
 class RandomSearch(Algorithm):
 
@@ -39,14 +37,9 @@ class RandomSearch(Algorithm):
     def initialize(self, f, area, seed):
         if seed is None:
             self._x = generate_random_point_in_rectangular(area)
-            self._f_x = f(self._x)
         else:
-            if isinstance(seed, list):
-                self._x = sorted(seed, key=lambda v: f(v))[0]
-                self._f_x = f(self._x)
-            else:
-                self._x = seed
-                self._f_x = f(self._x)
+            self._x = get_best_point_from_seed(seed)
+        self._f_x = f(self._x)
 
     def generate_new_point(self, f, area):
         x = self._x
