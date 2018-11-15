18c7e18f1dc561e56dd6b931e2aed49322c74fa4
Valentin Panovskiy <wol4aravio@yandex.ru>
2018-11-15 10:38:30 +0300

    algorithm template

--- osol/extremum/optimization/basic/algorithm.py
+++ osol/extremum/optimization/basic/algorithm.py
@@ -15,7 +15,7 @@ class Algorithm(ABC, metaclass=ContractsMeta):
 
     @abstractmethod
     @contract
-    def initialize(self, f, search_area):
+    def initialize(self, f, search_area, seed_state):
         """ Procedure that initializes optimization algorithm
 
             :param f: objective function
@@ -24,13 +24,16 @@ class Algorithm(ABC, metaclass=ContractsMeta):
             :param search_area: search area
             :type search_area: dict(str:tuple(number, number))
 
+            :param seed_state: seed state for the algorithm
+            :type seed_state: dict(str:*)|None
+
             :returns: current state of the algorithm
             :rtype: dict(str:*)
         """
 
     @abstractmethod
     @contract
-    def main_cycle(self, f, search_area, current_state):
+    def main_cycle(self, f, search_area, **kwargs):
         """ Main cycle of the algorithm
 
             :param f: objective function
@@ -39,8 +42,8 @@ class Algorithm(ABC, metaclass=ContractsMeta):
             :param search_area: search area
             :type search_area: dict(str:tuple(number, number))
 
-            :param current_state: current state of the algorithm
-            :type current_state: dict(str:*)
+            :param kwargs: current state of the algorithm
+            :type kwargs: dict(str:*)
 
             :returns: current state of the algorithm
             :rtype: dict(str:*)
@@ -48,7 +51,7 @@ class Algorithm(ABC, metaclass=ContractsMeta):
 
     @abstractmethod
     @contract
-    def terminate(self, f, search_area, current_state):
+    def terminate(self, f, search_area, **kwargs):
         """ Termination part of the algorithm
 
             :param f: objective function
@@ -57,15 +60,15 @@ class Algorithm(ABC, metaclass=ContractsMeta):
             :param search_area: search area
             :type search_area: dict(str:tuple(number, number))
 
-            :param current_state: current state of the algorithm
-            :type current_state: dict(str:*)
+            :param kwargs: current state of the algorithm
+            :type kwargs: dict(str:*)
 
             :returns: solution
             :rtype: vector
         """
 
     @contract
-    def optimize(self, f, search_area, max_iter=None):
+    def optimize(self, f, search_area, seed_state=None, max_iter=None):
         """ Optimization procedure
 
             :param f: objective function
@@ -74,16 +77,19 @@ class Algorithm(ABC, metaclass=ContractsMeta):
             :param search_area: search area
             :type search_area: dict(str:tuple(number, number))
 
+            :param seed_state: seed state for the algorithm
+            :type seed_state: dict(str:*)|None
+
             :param max_iter: maximum allowed number of iterations
             :type max_iter: int|None
 
             :returns: solution
             :rtype: vector
         """
-        current_state = self.initialize(f, search_area)
+        current_state = self.initialize(f, search_area, seed_state)
         try:
             for _ in range(max_iter or sys.maxsize):
-                current_state = self.main_cycle(f, search_area, current_state)
+                current_state = self.main_cycle(f, search_area, **current_state)
         except TerminatorExceptions.StopWorkException:
             pass
-        return self.terminate(f, search_area, current_state)
+        return self.terminate(f, search_area, **current_state)
