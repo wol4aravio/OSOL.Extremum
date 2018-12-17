cdfc6cf8fa472b3221a888aefe81cc186a15fbd1
Valentin Panovskiy <wol4aravio@yandex.ru>
2018-12-17 21:17:39 +0300

    parallel benchmarking

--- osol/extremum/optimization/benchmarks/optimization_benchmark.py
+++ osol/extremum/optimization/benchmarks/optimization_benchmark.py
@@ -1,8 +1,6 @@
 from abc import ABC, abstractmethod
 from contracts import contract, ContractsMeta
 
-from osol.extremum.etc.new_contracts import * # Inclusion of user defined contracts
-
 
 class OptimizationBenchmark(ABC, metaclass=ContractsMeta):
     """ Abstract class that describes desired interface to benchmark functions """
@@ -36,35 +34,3 @@ class OptimizationBenchmark(ABC, metaclass=ContractsMeta):
 
             :rtype: tuple(Vector, number)
         """
-
-
-@contract
-def benchmark_algorithm(algorithm, benchmarks, terminator, number_of_runs):
-    """ Performs benchmarking process
-
-    :param algorithm: target algorithm
-    :type algorithm: Algorithm
-
-    :param benchmarks: benchmarks to be used
-    :type benchmarks: dict(str:Benchmark)
-
-    :param terminator: termination criterion generator
-    :type terminator: *
-
-    :param number_of_runs: how many times the algorithm should be tested
-    :type number_of_runs: int
-
-    :returns: algorithm application results
-    :rtype: dict(str:tuple(list(tuple(Vector, number)),number))
-    """
-    results = dict()
-    for b_name, b_func in benchmarks.items():
-        search_area = b_func.search_area
-        bt = terminator(b_func)
-        results[b_name] = []
-        for _ in range(number_of_runs):
-            x = algorithm.optimize(bt, search_area)
-            y = b_func(x)
-            results[b_name].append((x, y))
-        results[b_name] = (results[b_name], b_func.solution[1])
-    return results
