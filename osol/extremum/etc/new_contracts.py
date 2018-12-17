ec09a98d474af3713fff74afd27c90059e170c65
Valentin Panovskiy <wol4aravio@yandex.ru>
2018-12-17 17:01:37 +0300

    attempt to fix

--- osol/extremum/etc/new_contracts.py
+++ osol/extremum/etc/new_contracts.py
@@ -1,14 +1,8 @@
 from contracts import new_contract
 from typing import Callable
 
-from osol.extremum.optimization.basic.vector import Vector
-from osol.extremum.optimization.basic.algorithm import Algorithm
-from osol.extremum.optimization.basic.terminator import Terminator
-from osol.extremum.optimization.benchmarks.optimization_benchmark import OptimizationBenchmark
 
-
-new_contract("Vector",
-             lambda v_: isinstance(v_, Vector))
+new_contract("Vector", lambda v: type(v).__name__ == "Vector")
 
 new_contract("valid_args_tuple",
              lambda v_: all([isinstance(v, int) or isinstance(v, float) for v in v_]))
@@ -23,6 +17,6 @@ new_contract("valid_constrain_tuples",
                              isinstance(max_, (int, float)) for k, (min_, max_) in v_]))
 
 new_contract("function", Callable)
-new_contract("Algorithm", Algorithm)
-new_contract("Terminator", Terminator)
-new_contract('Benchmark', OptimizationBenchmark)
+new_contract("Algorithm", lambda v: type(v).__name__ == "Algorithm")
+new_contract("Terminator", lambda v: type(v).__name__ == "Terminator")
+new_contract("Benchmark", lambda v: type(v).__name__ == "OptimizationBenchmark")
