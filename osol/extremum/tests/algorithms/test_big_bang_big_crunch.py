9c096d7c4e71fef135e6c0a86bd49413b3f77b54
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-02-05 09:13:49 +0300

    purge

--- osol/extremum/tests/algorithms/test_big_bang_big_crunch.py
+++
@@ -1,7 +0,0 @@
-from osol.extremum.algorithms.big_bang_big_crunch import BigBangBigCrunch
-from osol.extremum.tests.algorithms.verifier import verify
-
-
-def test():
-    bbbc = BigBangBigCrunch(number_of_points=10, scatter_parameter=1e1, quality_mode="dummy")
-    assert verify(bbbc)
--- osol/extremum/tests/algorithms/test_random_search.py
+++
@@ -1,7 +0,0 @@
-from osol.extremum.algorithms.random_search import RandomSearch
-from osol.extremum.tests.algorithms.verifier import verify
-
-
-def test():
-    rs = RandomSearch(radius=1e-1)
-    assert verify(rs)
