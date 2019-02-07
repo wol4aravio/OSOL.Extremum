155053a7704ede69b5c243aacaf1cbec097921d9
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-02-07 20:14:01 +0300

    FunctionWithCounter

--- osol/extremum/algorithm.py
+++ osol/extremum/algorithm.py
@@ -13,7 +13,29 @@ class Algorithm(ABC):
     def optimize(
             self,
             f: Callable[[BoxVector], IntervalNumber],
-            search_area: List[Tuple[float, float]]) -> np.ndarray:
+            search_area: List[Tuple[float, float]],
+            max_iterations: int) -> np.ndarray:
         ...
+
+
+    def optimize_max_runtime(
+            self,
+            f: Callable[[BoxVector], IntervalNumber],
+            search_area: List[Tuple[float, float]],
+            max_time: int) -> np.ndarray:
+        ...
+
+
+class FunctionWithCounter:
+    def __init__(self, f: Callable[[BoxVector], IntervalNumber], max_number_of_calls: int):
+        self._f = f
+        self._number_of_calls = 0
+        self._max_number_of_calls = max_number_of_calls
+
+    def __call__(self, x: BoxVector) -> IntervalNumber:
+        if self._number_of_calls >= self._max_number_of_calls:
+            raise Exception(f"Exceeded maximum number of allowed calls: {self._max_number_of_calls}")
+        self._number_of_calls += 1
+        return self._f(x)
 
 
