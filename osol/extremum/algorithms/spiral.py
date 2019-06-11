0ff44959876c8144003a8a2a3d687190ae7a5870
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-06-11 18:04:44 +0300

    seems to be done + default values

--- osol/extremum/algorithms/spiral.py
+++ osol/extremum/algorithms/spiral.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from osol.extremum.tools import wrap_function
 
+
 def _create_rotation_matrix(n, i, j, theta):
     R = np.eye(n)
     R[i, i] = R[j, j] = np.cos(theta)
@@ -20,32 +21,36 @@ def _create_composition_rotation_matrix(n, theta):
     return R
 
 
-def _make_step(x0, r, theta, x_origin=None):
+def _make_step(x0, r, theta, x_origin):
     n = len(x0)
-    R = _create_composition_rotation_matrix(n, theta)
-    S = r * R
-
-    if x_origin is None:
-        x_origin = np.zeros_like(x0)
-
+    S = r * _create_composition_rotation_matrix(n, theta)
     return np.dot(S, x0) - np.dot(S - np.eye(n), x_origin)
 
 
-def spiral_optimization(f, area, m, r, theta, max_iterations=None, max_evaluations=None, max_time=None):
+def _initialize(area, m, max_, min_):
+    return np.random.uniform(min_, max_, size=(area.shape[0], m))
+
+
+def _get_current_best(points, wrapped_function):
+    point_values = np.apply_along_axis(lambda x: wrapped_function(x), axis=0, arr=points)
+    x_best = points[:, np.argmin(point_values)]
+    return x_best
+
+
+def spiral_optimization(f, area, m=25, r=0.95, theta=(np.pi / 4), max_iterations=None, max_evaluations=None, max_time=None):
     min_ = area[:, 0].reshape(-1, 1)
     max_ = area[:, 1].reshape(-1, 1)
 
     wrapped_function, termination = wrap_function(f, max_iterations, max_evaluations, max_time)
-    points = np.random.uniform(min_, max_, size=(area.shape[0], m))
-    point_values = np.apply_along_axis(lambda x: wrapped_function(x), axis=0, arr=points)
-    x_origin = points[:, np.argmin(point_values)]
+    stepper = lambda x: _make_step(x, r, theta, x_best)
+
+    points = _initialize(area, m, max_, min_)
+    x_best = _get_current_best(points, wrapped_function)
 
     current_iteration = 0
     while not termination(current_iteration):
-        for i in range(m):
-            points[:, i] = _make_step(points[:, i], r, theta, x_origin)
+        points = np.apply_along_axis(stepper, axis=0, arr=points)
         points = np.clip(points, min_, max_)
-        point_values = np.apply_along_axis(lambda x: wrapped_function(x), axis=0, arr=points)
-        x_origin = points[:, np.argmin(point_values)]
+        x_best = _get_current_best(points, wrapped_function)
         current_iteration += 1
-    return x_origin, f(x_origin)
+    return x_best
