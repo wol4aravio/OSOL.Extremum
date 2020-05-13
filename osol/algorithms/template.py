8ae948eeb34e706ca21b6641fcf32b30cb0321dc
valentin.panovskiy <valentin.panovskiy@skillbox.ru>
2020-05-13 15:37:13 +0300

    template for optimization algorithms

---
+++ osol/algorithms/template.py
@@ -0,0 +1,37 @@
+"""Template for algorithm development"""
+
+from abc import ABC, abstractmethod
+
+from osol.algorithms.termination import TerminationException
+
+
+class Algorithm(ABC):
+    """Basic class for all optimization algorithms."""
+
+    def __init__(self, **parameters):
+        """General procedure of algorithm initialization"""
+        for name, value in parameters.items():
+            setattr(self, name, value)
+
+    @abstractmethod
+    def initialize(self, f, search_area):
+        """Initialization step."""
+
+    @abstractmethod
+    def iterate(self, f, search_area):
+        """Iterative step."""
+
+    @abstractmethod
+    def terminate(self, f, search_area):
+        """Termination step"""
+
+    def optimize(self, f, search_area, number_of_iterations):
+        """Optimization procedure."""
+        self.initialize(f, search_area)
+        for _ in range(number_of_iterations):
+            try:
+                self.iterate(f, search_area)
+            except TerminationException:
+                break
+        solution = self.terminate(f, search_area)
+        return solution
