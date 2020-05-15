07a26c718117eee3e3fc7343fc34e3d1232fddb4
valentin.panovskiy <valentin.panovskiy@skillbox.ru>
2020-05-16 02:24:46 +0300

    gradient descent

--- osol/algorithms/template.py
+++ osol/algorithms/template.py
@@ -30,3 +30,30 @@ class AlgorithmZeroOrder(ABC):
                 break
         solution = self.terminate(f, search_area)
         return solution
+
+
+class AlgorithmFirstOrder(ABC):
+    """Basic class for all optimization algorithms."""
+
+    @abstractmethod
+    def initialize(self, f, f_grad, search_area):
+        """Initialization step."""
+
+    @abstractmethod
+    def iterate(self, f, f_grad, search_area):
+        """Iterative step."""
+
+    @abstractmethod
+    def terminate(self, f, f_grad, search_area):
+        """Termination step"""
+
+    def optimize(self, f, f_grad, search_area, number_of_iterations):
+        """Optimization procedure."""
+        self.initialize(f, f_grad, search_area)
+        for _ in range(number_of_iterations):
+            try:
+                self.iterate(f, f_grad, search_area)
+            except TerminationException:
+                break
+        solution = self.terminate(f, f_grad, search_area)
+        return solution
