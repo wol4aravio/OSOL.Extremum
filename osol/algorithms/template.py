c31e25bde11b3f8011ee66fe23e528a66054b942
wol4aravio <panovskiy.v@yandex.ru>
2020-05-17 20:41:33 +0300

    trace save proc

--- osol/algorithms/template.py
+++ osol/algorithms/template.py
@@ -20,15 +20,36 @@ class AlgorithmZeroOrder(ABC):
     def terminate(self, f, search_area):
         """Termination step"""
 
-    def optimize(self, f, search_area, number_of_iterations):
+    def _get_trace_attributes(self):
+        names = [a for a in dir(self) if not a.startswith("_")]
+        names = [
+            a
+            for a in names
+            if a
+            not in ("trace", "initialize", "iterate", "terminate", "optimize")
+        ]
+        values = {n: getattr(self, n) for n in names}
+        self.trace.append(values)
+
+    def optimize(self, f, search_area, number_of_iterations, save_trace=False):
         """Optimization procedure."""
+        self.trace = None
         self.initialize(f, search_area)
+        if save_trace:
+            self.trace = list()
+            self._get_trace_attributes()
+        else:
+            self.trace = None
         for _ in range(number_of_iterations):
             try:
                 self.iterate(f, search_area)
+                if save_trace:
+                    self._get_trace_attributes()
             except TerminationException:
                 break
         solution = self.terminate(f, search_area)
+        if save_trace:
+            self._get_trace_attributes()
         return solution
 
 
