c9f104e14b1464767fa7686b4a84da7e8687a4c5
wol4aravio <panovskiy.v@yandex.ru>
2020-05-17 15:08:44 +0300

    smoke tests: L1

--- osol/smoke.py
+++ osol/smoke.py
@@ -3,6 +3,23 @@
 import numpy as np
 
 
+def generate_smoke_L1(n_dim, bounds=(-10, 10)):
+    """
+    Generate smoke function:
+    f(x) = sum(abs(x - x_sol))
+    """
+    x_solution = np.random.uniform(
+        low=bounds[0], high=bounds[1], size=(n_dim),
+    )
+
+    def f(x):
+        return np.sum(np.abs(x - x_solution))
+
+    f.solution = x_solution
+    f.search_area = np.full(shape=(n_dim, 2), fill_value=bounds)
+    return f
+
+
 def generate_smoke_L2(n_dim, bounds=(-10, 10)):
     """
     Generate smoke function:
--- tests/test_random_search.py
+++ tests/test_random_search.py
@@ -4,21 +4,40 @@
 import numpy.linalg as la
 import pytest
 from osol.algorithms.random_search import RandomSearch
-from osol.smoke import generate_smoke_L2
+from osol.smoke import generate_smoke_L1, generate_smoke_L2
 
 EPS = 1e-3
 TOL = 1e-2
 NUM_ITER = [int(n) for n in (1e3, 1e5, 1e7)]
 
-TEST_FUNCTIONS = (
+TEST_FUNCTIONS_L1 = (
+    generate_smoke_L1(n_dim=1),
+    generate_smoke_L1(n_dim=2),
+    generate_smoke_L1(n_dim=3),
+)
+
+TEST_FUNCTIONS_L2 = (
     generate_smoke_L2(n_dim=1)[0],
     generate_smoke_L2(n_dim=2)[0],
     generate_smoke_L2(n_dim=3)[0],
 )
 
 
-@pytest.mark.parametrize("f", TEST_FUNCTIONS)
-def test_algorithm(f):
+@pytest.mark.parametrize("f", TEST_FUNCTIONS_L1)
+def test_algorithm_L1(f):
+    """Smoke test."""
+    rs = RandomSearch(eps=EPS)
+    success = False
+    for num_iter in NUM_ITER:
+        sol = rs.optimize(f, f.search_area, num_iter)
+        success = success or la.norm(sol - f.solution) < TOL
+        if success:
+            break
+    assert success
+
+
+@pytest.mark.parametrize("f", TEST_FUNCTIONS_L2)
+def test_algorithm_L2(f):
     """Smoke test."""
     rs = RandomSearch(eps=EPS)
     success = False
