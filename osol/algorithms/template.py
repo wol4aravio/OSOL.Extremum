905cd76923fb113db2a391a006f5c91bbd547fb9
Valentin Panovskiy <panovskiy.v@yandex.ru>
2020-05-24 01:29:04 +0300

    Refactor (#127)
    * version up
    
    * refactor
    
    * second refactor
    
    * second refactor
    
    * third refactor
    
    * third refactor
    
    * fifth refactor

--- osol/algorithms/template.py
+++ osol/algorithms/template.py
@@ -5,23 +5,15 @@ from abc import ABC, abstractmethod
 from osol.algorithms.termination import TerminationException
 
 
-class AlgorithmZeroOrder(ABC):
-    """Basic class for all optimization algorithms."""
+class AlgorithmInterface(ABC):
+    """General functions that can be applied to any algorithm."""
 
-    @abstractmethod
-    def initialize(self, f, search_area):
-        """Initialization step."""
-
-    @abstractmethod
-    def iterate(self, f, search_area):
-        """Iterative step."""
-
-    @abstractmethod
-    def terminate(self, f, search_area):
-        """Termination step"""
-
-    def _get_trace_attributes(self):
+    def _get_trace_attributes(self, save_trace):
         """Save all non-private attributes."""
+        if not save_trace:
+            return
+        if not hasattr(self, "trace"):
+            setattr(self, "trace", list())
         names = [a for a in dir(self) if not a.startswith("_")]
         names = [
             a
@@ -30,117 +22,109 @@ class AlgorithmZeroOrder(ABC):
             not in ("trace", "initialize", "iterate", "terminate", "optimize")
         ]
         values = {n: getattr(self, n) for n in names}
-        self.trace.append(values)
+        setattr(self, "trace", getattr(self, "trace") + [values])
 
     def _log_attrs(self, verbose_attrs):
-        for attribute in verbose_attrs:
-            print(
-                "{name}: {value}".format(
-                    name=attribute, value=getattr(self, attribute),
-                )
-            )
-
-    def optimize(
-        self,
-        f,
-        search_area,
-        number_of_iterations,
-        save_trace=False,
-        verbose_attrs=None,
-    ):
-        """Optimization procedure."""
-        self.trace = None
-        self.initialize(f, search_area)
-        if save_trace:
-            self.trace = list()
-            self._get_trace_attributes()
-        else:
-            self.trace = None
         if verbose_attrs:
-            self._log_attrs(verbose_attrs)
+            for attribute in verbose_attrs:
+                print(
+                    "{name}: {value}".format(
+                        name=attribute, value=getattr(self, attribute),
+                    )
+                )
+
+
+class AlgorithmZeroOrder(AlgorithmInterface):
+    """Basic class for all optimization algorithms."""
+
+    @abstractmethod
+    def _initialize(self, f, search_area):
+        """Initialization step."""
+
+    @abstractmethod
+    def _iterate(self, f, search_area):
+        """Iterative step."""
+
+    @abstractmethod
+    def _terminate(self, f, search_area):
+        """Termination step"""
+
+    def initialize(self, f, search_area, save_trace, verbose_attrs):
+        """Initialization step."""
+        self._initialize(f, search_area)
+        self._get_trace_attributes(save_trace)
+        self._log_attrs(verbose_attrs)
+
+    def iterate(self, f, search_area, save_trace, verbose_attrs):
+        """Iterative step."""
+        self._iterate(f, search_area)
+        self._get_trace_attributes(save_trace)
+        self._log_attrs(verbose_attrs)
+
+    def terminate(self, f, search_area, save_trace, verbose_attrs):
+        """Termination step"""
+        solution = self._terminate(f, search_area)
+        self._get_trace_attributes(save_trace)
+        self._log_attrs(verbose_attrs)
+        return solution
+
+    def optimize(self, f, search_area, number_of_iterations, **kwargs):
+        """Optimization procedure."""
+        save_trace = kwargs.get("save_trace", False)
+        verbose_attrs = kwargs.get("verbose_attrs", None)
+        self.initialize(f, search_area, save_trace, verbose_attrs)
         for _ in range(number_of_iterations):
             try:
-                self.iterate(f, search_area)
-                if save_trace:
-                    self._get_trace_attributes()
-                if verbose_attrs:
-                    self._log_attrs(verbose_attrs)
+                self.iterate(f, search_area, save_trace, verbose_attrs)
             except TerminationException:
                 break
-        solution = self.terminate(f, search_area)
-        if save_trace:
-            self._get_trace_attributes()
-        if verbose_attrs:
-            self._log_attrs(verbose_attrs)
+        solution = self.terminate(f, search_area, save_trace, verbose_attrs)
         return solution
 
 
-class AlgorithmFirstOrder(ABC):
+class AlgorithmFirstOrder(AlgorithmInterface):
     """Basic class for all optimization algorithms."""
 
     @abstractmethod
-    def initialize(self, f, g, search_area):
+    def _initialize(self, f, g, search_area):
         """Initialization step."""
 
     @abstractmethod
-    def iterate(self, f, g, search_area):
+    def _iterate(self, f, g, search_area):
         """Iterative step."""
 
     @abstractmethod
-    def terminate(self, f, g, search_area):
+    def _terminate(self, f, g, search_area):
         """Termination step"""
 
-    def _get_trace_attributes(self):
-        """Save all non-private attributes."""
-        names = [a for a in dir(self) if not a.startswith("_")]
-        names = [
-            a
-            for a in names
-            if a
-            not in ("trace", "initialize", "iterate", "terminate", "optimize")
-        ]
-        values = {n: getattr(self, n) for n in names}
-        self.trace.append(values)
+    def initialize(self, f, g, search_area, save_trace, verbose_attrs):
+        """Initialization step."""
+        self._initialize(f, g, search_area)
+        self._get_trace_attributes(save_trace)
+        self._log_attrs(verbose_attrs)
 
-    def _log_attrs(self, verbose_attrs):
-        for attribute in verbose_attrs:
-            print(
-                "{name}: {value}".format(
-                    name=attribute, value=getattr(self, attribute),
-                )
-            )
+    def iterate(self, f, g, search_area, save_trace, verbose_attrs):
+        """Iterative step."""
+        self._iterate(f, g, search_area)
+        self._get_trace_attributes(save_trace)
+        self._log_attrs(verbose_attrs)
 
-    def optimize(
-        self,
-        f,
-        g,
-        search_area,
-        num_of_iterations,
-        save_trace=False,
-        verbose_attrs=None,
-    ):
+    def terminate(self, f, g, search_area, save_trace, verbose_attrs):
+        """Termination step"""
+        solution = self._terminate(f, g, search_area)
+        self._get_trace_attributes(save_trace)
+        self._log_attrs(verbose_attrs)
+        return solution
+
+    def optimize(self, f, g, search_area, num_of_iterations, **kwargs):
         """Optimization procedure."""
-        self.trace = None
-        self.initialize(f, g, search_area)
-        if save_trace:
-            self.trace = list()
-            self._get_trace_attributes()
-        else:
-            self.trace = None
-        if verbose_attrs:
-            self._log_attrs(verbose_attrs)
+        save_trace = kwargs.get("save_trace", False)
+        verbose_attrs = kwargs.get("verbose_attrs", None)
+        self.initialize(f, g, search_area, save_trace, verbose_attrs)
         for _ in range(num_of_iterations):
             try:
-                self.iterate(f, g, search_area)
-                if save_trace:
-                    self._get_trace_attributes()
-                if verbose_attrs:
-                    self._log_attrs(verbose_attrs)
+                self.iterate(f, g, search_area, save_trace, verbose_attrs)
             except TerminationException:
                 break
-        solution = self.terminate(f, g, search_area)
-        if save_trace:
-            self._get_trace_attributes()
-        if verbose_attrs:
-            self._log_attrs(verbose_attrs)
+        solution = self.terminate(f, g, search_area, save_trace, verbose_attrs)
         return solution
--- osol/algorithms/tools.py
+++ osol/algorithms/tools.py
@@ -1,9 +1,19 @@
 """Set of tools for optimization algorithms."""
 
 
+import random
+
 import numpy as np
 
 
 def bound_vector(x, bounds):
     """Push vector to be located incide specified bounds."""
     return np.clip(x, a_min=bounds[:, 0], a_max=bounds[:, 1])
+
+
+def generate_point_in_area(area):
+    """Generate point within area."""
+    x = np.zeros(area.shape[0])
+    for i in range(len(x)):
+        x[i] = random.uniform(area[i, 0], area[i, 1])
+    return x
