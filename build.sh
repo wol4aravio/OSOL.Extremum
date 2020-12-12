fac30aad356ff475e7779947e09ea05279791f12
valentin.panovskiy <valentin.panovskiy@skillbox.ru>
2020-12-12 23:43:30 +0300

    clean

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-VERSION=$(grep 'version \=' pyproject.toml | cut -d'=' -f2 | tr -d '"'| tr -d ' ')
-
-docker build . --tag osol:$VERSION
--- launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-       --publish 8501:8501 \
-       osol:${1}
