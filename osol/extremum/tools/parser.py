844b725ed7b4d283f4b32452fd646aa9eb2800ce
Valentin Panovskiy <panovskiy.v@yandex.ru>
2021-01-04 20:06:14 +0300

    Refactor (#136)
    * clean
    
    * fix
    
    * structure
    
    * first commit
    
    * first transfer
    
    * first transfer
    
    * algorithm tools
    
    * ignore
    
    * coverage
    
    * ignore files
    
    * parser
    
    Co-authored-by: valentin.panovskiy <valentin.panovskiy@skillbox.ru>

--- osol/extremum/tools/parser.py
+++ osol/extremum/tools/parser.py
@@ -14,9 +14,12 @@ class OptTask:
             parsed = json.load(opt_file)
         self._f = parse_latex(parsed["function"])
         self._vars = parsed["vars"]
+        self._n_vars = len(parsed["vars"])
 
     def __call__(self, *args, **kwargs):
         min_length = min(len(self._vars), len(args))
-        arg_values = {self._vars[i]: args[0] for i in range(min_length)}
+        arg_values = {self._vars[i]: args[i] for i in range(min_length)}
         arg_values = {**arg_values, **kwargs}
+        if len(arg_values) != self._n_vars:
+            raise ValueError("Not enough variables")
         return self._f.evalf(subs=arg_values)
