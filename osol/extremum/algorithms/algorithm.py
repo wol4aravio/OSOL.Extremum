2cff807ee3e0b8312fb3417ed1a7468f38730282
wol4aravio <panovskiy.v@yandex.ru>
2021-01-14 00:38:42 +0300

    termination via number of iterations

--- osol/extremum/algorithms/algorithm.py
+++ osol/extremum/algorithms/algorithm.py
@@ -3,7 +3,10 @@
 
 from abc import ABC, abstractmethod
 
-from osol.extremum.algorithms.termination import TerminationException
+from osol.extremum.algorithms.termination import (
+    TerminationException,
+    TerminationViaAlgorithmIterations,
+)
 
 
 class Algorithm(ABC):
@@ -31,12 +34,16 @@ class Algorithm(ABC):
 
     def optimize(self, f, search_area, number_of_iterations, **kwargs):
         """Optimization procedure."""
+        f_ = TerminationViaAlgorithmIterations(f, number_of_iterations)
         if not kwargs.get("skip_init", False):
-            self.initialize(f, search_area)
-        for _ in range(number_of_iterations):
+            self.initialize(f_, search_area)
+        iteration_id = 0
+        while True:
+            f_.set_current_iteration(iteration_id)
             try:
-                self.iterate(f, search_area)
+                self.iterate(f_, search_area)
             except TerminationException:
                 break
-        solution = self.terminate(f, search_area)
+            iteration_id += 1
+        solution = self.terminate(f_, search_area)
         return solution
--- osol/extremum/algorithms/termination.py
+++ osol/extremum/algorithms/termination.py
@@ -5,8 +5,28 @@ class TerminationException(Exception):
     """Termination error."""
 
 
+class TerminationViaAlgorithmIterations:
+    """Function caller with external iteration counter."""
+
+    def __init__(self, f, max_iterations):
+        """Initialize via max_iterations."""
+        self._f = f
+        self._current_iteration = 0
+        self._max_iterations = max_iterations
+
+    def set_current_iteration(self, iteration):
+        """Modifies current iteration."""
+        self._current_iteration = iteration
+
+    def __call__(self, x):
+        """Apply function to argument."""
+        if self._current_iteration >= self._max_iterations:
+            raise TerminationException()
+        return self._f(x)
+
+
 class TerminationViaMaxCalls:
-    """Function caller with limit.."""
+    """Function caller with limit."""
 
     def __init__(self, f, max_calls):
         """Initialize via max_calls."""
