ca1f4de9bac171053578898487d2b183761e1bb1
wol4aravio <panovskiy.v@yandex.ru>
2021-03-04 02:40:53 +0300

    fix rs tests

--- tests/osol/extremum/algorithms/test_random_search.py
+++ tests/osol/extremum/algorithms/test_random_search.py
@@ -6,4 +6,4 @@ from osol.extremum.tools.testing import smoke_check
 
 @pytest.mark.parametrize("_", range(10))
 def test_random_search(_):
-    assert smoke_check(RandomSearch(1e-1))
+    assert smoke_check(RandomSearch(1e-1), number_of_iterations=2500)
