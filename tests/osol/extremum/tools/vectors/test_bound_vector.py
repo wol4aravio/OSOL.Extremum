76a69e22cc961d3cefd3a8dec579c3f04248180c
wol4aravio <panovskiy.v@yandex.ru>
2021-03-04 00:55:35 +0300

    tests

--- tests/osol/extremum/tools/vectors/test_bound_vector.py
+++ tests/osol/extremum/tools/vectors/test_bound_vector.py
@@ -1,10 +1,11 @@
-"""Tests for algorithm tools."""
+"""Tests for algorithm tools: vector bound."""
 
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from osol.extremum.tools.vectors import bound_vector
+from osol.extremum.tools.vectors import bound_vector, generate_point_in_area
 
 
 def test_bound_vector_1():
@@ -26,3 +27,13 @@ def test_bound_vector_3():
     v = np.array([5, -5, 0])
     bounds = np.array([[-2, 2] * len(v)])
     npt.assert_equal(np.array([2, -2, 0]), bound_vector(v, bounds))
+
+
+@pytest.mark.parametrize("_", range(100))
+def test_bound_vector_N(_):
+    """Run procedural generated tests."""
+    search_area = np.array([[-10, 10], [-10, 10], [-10, 10]])
+    bounds = np.array([[-5, 5], [-5, 5], [-5, 5]])
+    vector = generate_point_in_area(search_area)
+    vector = bound_vector(vector, bounds)
+    assert ((bounds[:, 0] <= vector) & (vector <= bounds[:, 1])).all()
---
+++ tests/osol/extremum/tools/vectors/test_generate_point_in_area.py
@@ -0,0 +1,15 @@
+"""Tests for algorithm tools: vector generation."""
+
+
+import numpy as np
+import pytest
+
+from osol.extremum.tools.vectors import generate_point_in_area
+
+
+@pytest.mark.parametrize("_", range(100))
+def test_generate_point_in_area(_):
+    """Run procedural generated tests."""
+    search_area = np.array([[-10, 10], [-10, 10]])
+    vector = generate_point_in_area(search_area)
+    assert ((search_area[:, 0] <= vector) & (vector <= search_area[:, 1])).all()
