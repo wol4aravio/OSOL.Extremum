70d19ac37e7c1551c55c0328ee3c0231f53274ae
wol4aravio <panovskiy.v@yandex.ru>
2021-03-05 00:30:51 +0300

    better coverage

--- tests/osol/extremum/algorithms/test_random_search.py
+++ tests/osol/extremum/algorithms/test_random_search.py
@@ -6,4 +6,11 @@ from osol.extremum.tools.testing import smoke_check
 
 @pytest.mark.parametrize("_", range(10))
 def test_random_search(_):
-    assert smoke_check(RandomSearch(1e-1), number_of_iterations=2500)
+    algorithm = RandomSearch(1e-1)
+    assert smoke_check(algorithm, number_of_iterations=2500)
+    try:
+        state = algorithm.serialize()
+        algorithm.deserialize(state)
+        assert True
+    except Exception:
+        assert False
