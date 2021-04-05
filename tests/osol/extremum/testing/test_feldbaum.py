abf5ef37598f8e225dc8aac6db504f4b4181c5ef
wol4aravio <panovskiy.v@yandex.ru>
2021-04-05 23:36:11 +0300

    fix tests

--- tests/osol/extremum/testing/test_feldbaum.py
+++ tests/osol/extremum/testing/test_feldbaum.py
@@ -19,3 +19,24 @@ def test_2(_):
     f = FeldbaumTestFunctions(N, dim=2)
     min_b = np.min(f._b)
     npt.assert_almost_equal(np.min([f(f._c[i, :]) for i in range(N)]), min_b)
+
+
+def test_3():
+    a = np.array([[1, 2], [3, 4]])
+    b = np.array([[5], [6]])
+    c = np.array([[7, 8], [9, 10]])
+    p = np.array([[0.5, 2], [2, 0.5]])
+    f = FeldbaumTestFunctions(N=2, dim=2, random=False, a=a, b=b, c=c, p=p)
+    npt.assert_almost_equal(f(np.array([7, 8])), 5.0)
+
+    x = np.array([1, 2])
+
+    part_11 = a[0, 0] * np.abs(x[0] - c[0, 0]) ** p[0, 0]
+    part_12 = a[0, 1] * np.abs(x[1] - c[0, 1]) ** p[0, 1]
+    part_1 = part_11 + part_12 + b[0, 0]
+
+    part_21 = a[1, 0] * np.abs(x[0] - c[1, 0]) ** p[1, 0]
+    part_22 = a[1, 1] * np.abs(x[1] - c[1, 1]) ** p[1, 1]
+    part_2 = part_21 + part_22 + b[1, 0]
+
+    npt.assert_almost_equal(f(x), np.min([part_1, part_2]))
