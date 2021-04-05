8d9d8fa0b52cfa0aceea0b9ad0b4815e33e418bb
wol4aravio <panovskiy.v@yandex.ru>
2021-04-05 23:25:46 +0300

    tests for Feldbaum method

---
+++ tests/osol/extremum/testing/test_feldbaum.py
@@ -0,0 +1,21 @@
+# pylint: disable=protected-access
+
+import numpy as np
+import numpy.testing as npt
+import pytest
+
+from osol.extremum.algorithms.testing import FeldbaumTestFunctions
+
+
+@pytest.mark.parametrize("_", range(10))
+def test_1(_):
+    f = FeldbaumTestFunctions(N=1, dim=2)
+    npt.assert_almost_equal(f(f._c[0, :]), f._b)
+
+
+@pytest.mark.parametrize("_", range(10))
+def test_2(_):
+    N = 10
+    f = FeldbaumTestFunctions(N, dim=2)
+    min_b = np.min(f._b)
+    npt.assert_almost_equal(np.min([f(f._c[i, :]) for i in range(N)]), min_b)
