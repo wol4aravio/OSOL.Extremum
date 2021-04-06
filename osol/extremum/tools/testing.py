e6d647eebd07bc1bb68ddbc9f2b7ef98e35eb9bd
wol4aravio <panovskiy.v@yandex.ru>
2021-04-06 23:42:18 +0300

    RS: done

--- osol/extremum/tools/testing.py
+++ osol/extremum/tools/testing.py
@@ -8,48 +8,44 @@ class FeldbaumTestFunctions:
     def _generate_function(a, b, c, p):
         return lambda x: np.sum(a * np.abs(x - c) ** p) + b
 
-    def __init__(self, N, dim, random=True, a=None, b=None, c=None, p=None):
-        self._N = N
-        self._dim = dim
-        if random:
-            self._a = np.random.uniform(low=0.0, high=5.0, size=(N, dim))
-            self._b = np.random.uniform(low=-10.0, high=10.0, size=(N, 1))
-            self._c = np.random.uniform(low=-10.0, high=10.0, size=(N, dim))
-            self._p = np.random.uniform(low=0.0, high=5.0, size=(N, dim))
-        else:
-            self._a = a
-            self._b = b
-            self._c = c
-            self._p = p
+    def _compose_function_parts(self):
         self._functions = [
             FeldbaumTestFunctions._generate_function(
                 self._a[i, :], self._b[i, :], self._c[i, :], self._p[i, :]
             )
-            for i in range(N)
+            for i in range(self._N)
         ]
+
+    def __init__(self, N, dim):
+        self._N = N
+        self._dim = dim
+        self._a = np.random.uniform(low=0.0, high=5.0, size=(N, dim))
+        self._b = np.random.uniform(low=-10.0, high=10.0, size=(N, 1))
+        self._c = np.random.uniform(low=-10.0, high=10.0, size=(N, dim))
+        self._p = np.random.uniform(low=0.0, high=5.0, size=(N, dim))
+        self._functions = None
+        self._compose_function_parts()
 
     def __call__(self, x):
         return np.min([f(x) for f in self._functions])
 
 
-def smoke_check(algorithm, number_of_iterations):
+def smoke_check(algorithm, number_of_iterations, eps=1e-2):
     left, right = -10, 10
     search_area = np.array([[left, right], [left, right]])
     x_opt = np.random.uniform(left, right, 2)
-    eps = 1e-2
     x_found = algorithm.optimize(
         lambda x: np.sum((x - x_opt) ** 2), search_area, number_of_iterations
     )
     return np.linalg.norm(x_found - x_opt) < eps
 
 
-def feldbaum_check(algorithm, number_of_iterations):
+def feldbaum_check(algorithm, number_of_iterations, eps=1e-2):
     left, right = -10, 10
     search_area = np.array([[left, right], [left, right]])
-    eps = 1e-2
     f = FeldbaumTestFunctions(N=3, dim=2)
-    y_best = np.min(f._b)
-    y_best_loc = np.where(f._b == y_best)[0][0]
-    x_opt = f._c[y_best_loc, :]
+    y_opt = np.min(f._b)
+    y_opt_loc = np.where(f._b == y_opt)[0][0]
+    x_opt = f._c[y_opt_loc, :]
     x_found = algorithm.optimize(f, search_area, number_of_iterations)
     return np.linalg.norm(x_found - x_opt) < eps
--- tests/osol/extremum/algorithms/test_random_search.py
+++ tests/osol/extremum/algorithms/test_random_search.py
@@ -1,13 +1,25 @@
 import pytest
 
 from osol.extremum.algorithms.random_search import RandomSearch
-from osol.extremum.tools.testing import smoke_check
+from osol.extremum.tools.testing import feldbaum_check, smoke_check
 
 
 @pytest.mark.parametrize("_", range(10))
 def test_random_search_smoke(_):
-    algorithm = RandomSearch(1e-1)
-    assert smoke_check(algorithm, number_of_iterations=2500)
+    algorithm = RandomSearch(1.0)
+    assert smoke_check(algorithm, number_of_iterations=1000)
+    try:
+        state = algorithm.serialize()
+        algorithm.deserialize(state)
+        assert True
+    except Exception:
+        assert False
+
+
+@pytest.mark.parametrize("_", range(10))
+def test_random_search_Feldbaum(_):
+    algorithm = RandomSearch(1.0)
+    assert feldbaum_check(algorithm, number_of_iterations=1000, eps=1e-1)
     try:
         state = algorithm.serialize()
         algorithm.deserialize(state)
