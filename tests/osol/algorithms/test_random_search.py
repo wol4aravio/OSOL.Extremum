29673f7c3831dc596fbaf564f41d87d7f22cf05d
wol4aravio <panovskiy.v@yandex.ru>
2021-04-07 21:10:40 +0300

    rename test

--- tests/osol/algorithms/test_random_search.py
+++ tests/osol/algorithms/test_random_search.py
@@ -5,7 +5,7 @@ from osol.tools.testing import smoke_check
 
 
 @pytest.mark.parametrize("_", range(10))
-def test_random_search_smoke(_):
+def test_smoke(_):
     algorithm = RandomSearch(1.0)
     assert smoke_check(algorithm, number_of_iterations=1000)
     try:
